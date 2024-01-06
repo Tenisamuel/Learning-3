d226e9a28815d94f8bb90c99f4e64744904bed50
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-01-06 17:37:43 +0000

    Gorilla

---
+++ Teni.py
@@ -0,0 +1,3 @@
+print("The guide told us Stay very still!")
+print("Gorilla's are the largest living primates")
+print("The scientific name for the Western Lowland\ngorilla is Gorilla gorilla gorilla ")
