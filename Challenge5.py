c44234716544e9fa8f011fa111df1cd2826a0a04
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-01-07 22:07:33 +0000

    product of code

---
+++ Challenge5.py
@@ -0,0 +1,5 @@
+#Copy the program above and spot the error in the code. if you have made an error in your program, click OK in the message
+#box, correct the error, save the program by pressing Ctrl-s, and test it again by pressing f5.
+print("Hi there!\n")
+firstname = input("What is your name?")
+print("Hello", firstname)
