c09ced111598a3ef266b1164832073ef494f7508
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-01-07 22:28:27 +0000

    command

---
+++ Challenge6.py
@@ -0,0 +1,6 @@
+#Using the example shown in the screenshot above as a guide, write, save, and run a program to ask the user to input the names of three animals.
+#Print the animal names out once all they have all been entered.
+#Include two comments to document the program name and describe what it does.
+print("Hello there\n")
+Animalnames = input("Can you name 3 animals? ")
+print("Correct!")
