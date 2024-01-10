de73af34b5668667e3d85cdb56e224f98c21e6dc
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-01-10 21:20:31 +0000

    Result

---
+++ Challenge8.py
@@ -0,0 +1,8 @@
+#Identify and correct the syntax errors in the program below which is saved in the Starred challenges (incomplete) folder.
+#Correct the errors,test the program and save it in your own folder.The corrected program is in the Challenge solutions folder.
+print("This program contains syntax errors\n")
+favourite_animal = "Giraffe"
+scariestAnimal = "lion"
+print("favourte animal", favourite_animal )  
+"\n"
+"(Scariest animal....", scariestAnimal
---
+++ challenge9.py
@@ -0,0 +1,5 @@
+myBestScore = 40
+Name = 14
+cost_of_meal = 80
+print84 = 26
+_1234 = 12
