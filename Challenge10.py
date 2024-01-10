015af67e271f7aa5bec3924aeeb9181c741b36be
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-01-10 21:39:52 +0000

    Result

---
+++ Challenge10.py
@@ -0,0 +1,6 @@
+#Predicted what is printed when python runs these statements.
+x = 5
+y = 10
+x = y 
+y = 5
+print(x,y)
--- challenge9.py
+++ challenge9.py
@@ -1,3 +1,4 @@
+#Which of the following are valid variable names?
 myBestScore = 40
 Name = 14
 cost_of_meal = 80
