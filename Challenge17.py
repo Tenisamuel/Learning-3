ab6c26197ea76da29d3a9728ddff5cb9e25d7ddb
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-01-13 16:38:58 +0000

    Result

---
+++ Challenge17.py
@@ -0,0 +1,7 @@
+#Complete the program challenge 17 Divide books among children incomplete in the python programs/Starred 
+#Challenges(incomplete)
+#folder The program is designed to specify the total nu,ber of books available to be given to a group of 
+#children. it then caculates
+#And prints how many books each child will receive and how many will be left over.
+books = 650
+children = 173
--- Challenge8.py
+++ Challenge8.py
@@ -1,8 +1,7 @@
 #Identify and correct the syntax errors in the program below which is saved in the Starred challenges (incomplete) folder.
 #Correct the errors,test the program and save it in your own folder.The corrected program is in the Challenge solutions folder.
-print("This program contains syntax errors\n")
+print("This program  does not contains syntax errors\n")
 favourite_animal = "Giraffe"
 scariestAnimal = "lion"
-print("favourte animal", favourite_animal )  
-("\n")
-"(Scariest animal....", scariestAnimal
+print("favourte animal", favourite_animal,  
+    "\nScariestAnimal....",scariestAnimal)
