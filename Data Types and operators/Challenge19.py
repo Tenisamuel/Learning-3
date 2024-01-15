b0b09e3c83684bdbef156a1618afbac15d260378
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-01-15 20:32:50 +0000

    Resullt

---
+++ Data Types and operators/Challenge19.py
@@ -0,0 +1,10 @@
+#A personal pedometer tracks and records the distance walked each day by the wearer.
+#Choose meaningful names (identifers) for two variables holding the number of a miles walked
+#on each day for two days,Monday and Tuesday.Assign values(each with a decimal point) to
+#these variables.
+
+#Write a single statement to caculate and print the total miles walked.
+identifier = 32.65 
+identfier2 = 28.21
+Monday_Tuesday =  identifier+identfier2
+print("Today miles walked on Monday and Tuesday =", identifier+identfier2) 
