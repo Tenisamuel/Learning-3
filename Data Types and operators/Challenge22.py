02cbb7616494d47b2a959bb5637bfbce0947e9a3
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-01-17 20:30:43 +0000

    Answers

--- Data Types and operators/Challenge22.py
+++ Data Types and operators/Challenge22.py
@@ -3,3 +3,4 @@ flight = 91.25
 train = 11.80
 total = cabfare + flight + train
 print("Total travel expenses =", total)
+
---
+++ Data Types and operators/Challenge24.py
@@ -0,0 +1,4 @@
+#For each of the following variables, state wheather they need to be of string.integer or float type.
+numberOfSiblings = int("4")
+name = str("Teni")
+price = float("5.50")
