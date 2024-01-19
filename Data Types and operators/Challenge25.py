8c177e01310c03ae7f5b574bfe645d84b947498a
Los-merengue <michaels4uandme@gmail.com>
2024-01-19 23:44:04 +0300

    Added challenge25.py

--- Data Types and operators/Challenge25.py
+++ Data Types and operators/Challenge25.py
@@ -1,5 +1,18 @@
 #Write statements to ask a truck driver to enter the weight of thier goods in pound(lb), convert
-#the data to an integer and add24,350 lbs to it to adjust for the wright on the truck. print out
+#the data to an integer and add 24,340 lbs to it to adjust for the weight on the truck. print out
 #the combined(laden) weight e.g
 #The laden weight of the truck  is .... lbs
 #Convert the weight to kilos and output the result as an integer. There are 2.2 lbs to a kg.
+
+goodsWeight = input("Enter the Weight of Goods in Pounds: \n")
+integer_of_goodWeight = int(goodsWeight)
+
+answer = integer_of_goodWeight + 24350
+
+print("The total answer in pounds is:\n", answer, "lbs")
+
+#This section of the code will converts the lbs to kg
+
+kg_answer = answer/2.2
+
+print("The answer of the weight in kg is:\n", kg_answer, "kg")
