835c0d3a03501574c872c22f73927f5b2eafd132
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-01-19 20:25:34 +0000

    Comment

---
+++ Data Types and operators/Challenge25.py
@@ -0,0 +1,5 @@
+#Write statements to ask a truck driver to enter the weight of thier goods in pound(lb), convert
+#the data to an integer and add24,350 lbs to it to adjust for the wright on the truck. print out
+#the combined(laden) weight e.g
+#The laden weight of the truck  is .... lbs
+#Convert the weight to kilos and output the result as an integer. There are 2.2 lbs to a kg.
