ed89a649cf00e508d42bdc8a077fd3a2750fb7ae
Los-merengue <michaels4uandme@gmail.com>
2024-01-20 20:14:35 +0300

    Modify the challenge25.py file

---
+++ Data Types and operators/ProvChallenge.py
@@ -0,0 +1,5 @@
+#Write statements to ask a software engineer to enter the size of a memory in kB, convert
+#the data to an integer and add 12,564 to it to adjust for the memory size. print out
+#the combined result e.g
+#The combined size of the memory is .... kB
+#Convert the kB to MB and output the result as an integer. There are 1024kB to a MB.
