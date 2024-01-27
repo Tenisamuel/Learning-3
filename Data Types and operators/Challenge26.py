0a27040df1fc7a6383a5c4c1b84d44987974c0a0
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-01-27 17:48:47 +0000

    Answer to number 26

--- Data Types and operators/Challenge26.py
+++ Data Types and operators/Challenge26.py
@@ -2,3 +2,11 @@
 #the cab driver inputs the number of km from the airport to a hotel, and the computer caculates and outputs
 #the fare.
 
+basic_fee = 5
+kilometer = float(input("Enter the distance in kilometers from the airport to hotel: "))
+
+print(kilometer)
+
+fare_price = 1.30 * kilometer + basic_fee
+
+print("The Distance in kilometers from the airport to the hotel is", fare_price)
