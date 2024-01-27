cc9331c707243ff9653a9c6f76190c7c2dd0a665
Los-merengue <michaels4uandme@gmail.com>
2024-01-27 20:40:56 +0300

    Modify the question

--- Data Types and operators/Challenge26.py
+++ Data Types and operators/Challenge26.py
@@ -1,13 +1,4 @@
-#A taxi company charges basic fee of $4.00 plus $0.80 per km.To estimate the cost of a taxi fare,
+#A taxi company charges basic fee of $5.00 plus $1.30 per km.To estimate the cost of a taxi fare,
 #the cab driver inputs the number of km from the airport to a hotel, and the computer caculates and outputs
 #the fare.
 
-basic_fee = 4
-
-km = float(input("Enter the distance in kilometer from the airport to hotel: "))
-
-print(km)
-
-fareprice = 0.80*km + basic_fee
-
-print("The fare price for this journey is given as: ", fareprice)
