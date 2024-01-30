0b3bb7bd3d2ada9991acc858c7a47e4c1d96c5d2
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-01-30 20:17:37 +0000

    Sum of integer

---
+++ Data Types and operators/Challenge28.py
@@ -0,0 +1,9 @@
+#Assign values to two integer variables num1 and num2. Write a single print statement to add the two
+#numbers and print, for example, The sum of 24 and 3 is 27. Test your answer in the python shell window.
+
+num1 = 32
+num2 = 64
+
+num3 = 32+64
+
+print("The sum of 32 and 64 is",num3)
