ae5e63cce969eb1774641d3a8031721197be8292
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-02-02 21:12:03 +0000

    Answers

---
+++ Data Types and operators/Challenge29.py
@@ -0,0 +1,9 @@
+#Write a program to accept the name of a student, e.g Ron.Then write an input statement.
+#which displays the prompt:  "Enter mark out of 20 scored by ron: ".
+
+#in the print statement,multiply the mark by 5 to display the mark as a percentage, e.g.
+#Ron scored 75 percent.
+
+Score1 = float(input("Enter mark out of 20 scored by ron:\n"))
+Score2 = (Score1*5)
+print(Score2)
