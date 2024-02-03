ca8c2850b5131530ee2ecc2349a896e3266663d9
Los-merengue <michaels4uandme@gmail.com>
2024-02-03 21:45:48 +0300

    Solution to problem

--- Data Types and operators/Challenge31.py
+++ Data Types and operators/Challenge31.py
@@ -6,16 +6,22 @@
 
 #print the result.
 
-#time_hours = int(input("Enter the time in hours: \n"))
+time_hours = int(input("Enter the time in hours: \n"))
 
-#time_min = int(input("Enter the time in minutes: \n"))
+time_min = int(input("Enter the time in minutes: \n"))
 
 #convert_hours_to_minutes = (time_hours * 60) + time_min
 
-#total_time_in_hours = (time_min / 60) + time_hours
-#print("The total time in minutes is given as:", convert_hours_to_minutes,"minutes")
-#print("The total time in hours is given as", total_time_in_hours , "hours")
+total_time_in_hours = (time_min / 60) + time_hours
 
-Total_time_in_hours = float(input("Enter the total amount of time in hours: \n"))
-Distance_divide_time = (35 / Total_time_in_hours  )
-print("The total speed the cyclist used in the race is", Distance_divide_time)
+#print("The total time in minutes is given as:", convert_hours_to_minutes,"minutes")
+
+print("The total time in hours is given as", total_time_in_hours , "hours")
+
+#Total_time_in_hours = float(input("Enter the total amount of time in hours: \n"))
+#Distance_divide_time = (35 / Total_time_in_hours  )
+#print("The total speed the cyclist used in the race is", Distance_divide_time)
+
+speed_of_journey = 35 / total_time_in_hours
+
+print("The speed of the journey is given as:", speed_of_journey , "miles/hr")
