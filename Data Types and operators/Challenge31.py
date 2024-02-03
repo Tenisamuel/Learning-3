b16ea68cc29271c88abf7536838c5f0482071768
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-02-03 18:13:04 +0000

    answer

---
+++ Data Types and operators/Challenge31.py
@@ -0,0 +1,8 @@
+#A cyclist training for a race records his times over 35 miles for two consecutive days. His times are 2 hours
+#58 minutes and 3 Hours 10 minutes.
+
+#Complete the Python program Challenge 15 average cyclist time incomplete in the Starred challenges
+#(incomplete) folder to caculate his average time in minutes, Convert the average to hours and minutes and 
+
+#print the result.
+
