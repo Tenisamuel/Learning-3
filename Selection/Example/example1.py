d83682eb9eb14e82b6c02f0dcae16f87abc6fffc
Los-merengue <michaels4uandme@gmail.com>
2024-02-12 22:35:51 +0300

    Refractor the example1.py file

--- Selection/Example/example1.py
+++ Selection/Example/example1.py
@@ -1,2 +1,11 @@
-#The program inputs the currenyt battery charge on a mobile phone. if it is below 20%, it prints "Low battery.
+#The program inputs the current battery charge on a mobile phone. if it is below 20%, it prints "Low battery.
 #Charge required.". if it is between 21% and 99% it prints "Battery charge OK.". if it is 100% "Fully Charged.".
+
+battery_input = int(input("Enter the percentage of the Battery: \n"))
+
+if battery_input < 20:
+    print("Low Battery, Charge required")
+elif battery_input == 100:
+    print("Fully Charged")
+else:
+    print("Battery charge OK")
