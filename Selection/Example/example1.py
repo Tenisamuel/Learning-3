cbf40be72252227ec65e5d69a5e5c9eeec0adbd9
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-02-12 19:20:12 +0000

    The use of elif, if and else.

---
+++ Selection/Example/example1.py
@@ -0,0 +1,2 @@
+#The program inputs the currenyt battery charge on a mobile phone. if it is below 20%, it prints "Low battery.
+#Charge required.". if it is between 21% and 99% it prints "Battery charge OK.". if it is 100% "Fully Charged.".
