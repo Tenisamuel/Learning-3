3ded2c3b612d20756479880f650f2a083f91ca39
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-02-17 18:02:19 +0000

    Food program

--- Selection/Example/example2.py
+++ Selection/Example/example2.py
@@ -8,3 +8,29 @@
 # Usman -> (Beans and Yam)
 # Nasiru -> (Potatoes and Eggs)
 # Other people that is not part of the family will be asked to leave the program 
+
+Family_Member = input("Hello,Who are you, Can you tell me your name?\n")
+
+if Family_Member == "Teni":
+    print("Hello Teni, I have been instructed to serve you rice and beans")
+
+elif Family_Member == "Micheal":
+    print("Hello Micheal, I have been instructed to make you Egg and yam")
+
+elif Family_Member == "Richard":
+    print("Hi Richard, I have been asked to prepare you ogbono and pounded yam")
+
+elif Family_Member == "Pelumi":
+    print("Hello Pelumi, I have Beans and bread for you to eat")
+
+elif Family_Member == "Desmond":
+    print("Hello Desmond, I have been instructed to give you Egusi, Fish and Eba")
+
+elif Family_Member == "Usman":
+    print("Hi Usman,  I have been asked to make you Beans and Yam")
+
+elif Family_Member == "Nasiru":
+    print("Hello Nasiru,I have been asked to serve you potatoes and eggs")
+
+else:
+    print("You are not part of this family please leave this program")
