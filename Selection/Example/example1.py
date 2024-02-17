d85886a13b459f0eb9bf4b375a34d2200aa57f1a
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-02-17 17:19:34 +0000

    Use of if elif and else statement

--- Selection/Example/example1.py
+++ Selection/Example/example1.py
@@ -1,5 +1,14 @@
-#The program inputs the current battery charge on a mobile phone. if it is below 25%, it prints "Low battery,
-#Please Charge.". if it is between 25% and 99% it prints "Good Charged Battery.". if it is above 100% "Fully Done.".
+#The program inputs the current battery charge on a mobile phone. if it is below 10%, it prints "Low battery,
+#Please Charge.". if it is between 25% and 99% it prints "Good Charged Battery.". if it is above 150% "Fully Done.".
 
 battery_input = int(input("Enter the percentage of the Battery: \n"))
 
+if battery_input < 10:
+    print("Low battery")
+
+elif battery_input > 150:
+   print("Fully Charged")
+
+else:
+  print("Good Charged battery")
+    
