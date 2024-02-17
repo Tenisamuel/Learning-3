24ac3ccad1c44486489e7051aefd93be8a7162c6
Los-merengue <michaels4uandme@gmail.com>
2024-02-17 20:34:51 +0300

    Added the question

---
+++ Selection/Example/example2.py
@@ -0,0 +1,10 @@
+# Write a simple program that prepares the food menu of a family member has been produced. The name of the family member are given as
+#Teni, Michael, Richard, Pelumi, Desmond, Usman, Nasiru. The program prepares the food menu using the table given below.
+# Teni -> (Rice and Beans)
+# Michael -> (Egg and Yam)
+# Richard -> (Ogbono and Pounded Yam)
+# Pelumi -> (Beans and Bread)
+# Desmond -> (Egusi and Fish and Eba)
+# Usman -> (Beans and Yam)
+# Nasiru -> (Potatoes and Eggs)
+# Other people that is not part of the family will be asked to leave the program 
