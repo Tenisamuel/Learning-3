560c3d74c0913e826371f7a7789b21c3d2113a53
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-02-24 18:01:42 +0000

    results

---
+++ Selection/Challenges/Challenge40.py
@@ -0,0 +1,4 @@
+#in the python shell, write a statemnt which evaluates and prints a boolean expression equivalent to 
+
+#237 // 17 is equal to 237 / 17
+ #two = 237 // 17; three = 237 / 17; print(two == three)
