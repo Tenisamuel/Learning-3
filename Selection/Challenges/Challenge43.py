0b8416acc972455608a9f0889a0377f95b322ffb
Los-merengue <michaels4uandme@gmail.com>
2024-03-02 19:42:49 +0300

    Modify the challenge43.py script

--- Selection/Challenges/Challenge43.py
+++ Selection/Challenges/Challenge43.py
@@ -2,12 +2,16 @@
 #if the two weights are equal, print "The car is balanced". if the weights are not equal, print 
 #"The car is not balanced." include at least two comments in your program.
 
-#This if statement helps to compare two or more things together.
-#
+
+
+#These are the variable for this task
 
 left_weights = float(input("What is the weight of the left hand weight?\n"))
 
 right_weights = float(input("What is the weight of the right hand weight?\n"))
+
+
+#This if statement helps to compare the right and left weight.
 
 if left_weights == right_weights:
     print("The car is balanced")
