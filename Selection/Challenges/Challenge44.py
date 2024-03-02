f0c466a872c3b3a6664733fef008be2db6ba273d
Los-merengue <michaels4uandme@gmail.com>
2024-03-02 20:08:18 +0300

    Modify the question

--- Selection/Challenges/Challenge44.py
+++ Selection/Challenges/Challenge44.py
@@ -1,6 +1,6 @@
-#Complete the program below to check tyre pressure. if it is between 28 psi and 32 psl, print "Tyre pressure OK"
+#Complete the program below to check tyre pressure. if it is between 39 psi and 45 psl, print "Tyre pressure OK"
 #Otherwise, print "Tyre pressure is too high or too low"
 
+#This is the variable of the pressure
 pressure = int(input("Type tyre pressure in psi: "))
 
-if pressure == 
