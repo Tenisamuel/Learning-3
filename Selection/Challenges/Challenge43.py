3d2ae93c2d1fda67c618c8e5ce781c019f1d72f4
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-03-02 16:40:12 +0000

    The use of if statements

--- Selection/Challenges/Challenge43.py
+++ Selection/Challenges/Challenge43.py
@@ -2,6 +2,9 @@
 #if the two weights are equal, print "The car is balanced". if the weights are not equal, print 
 #"The car is not balanced." include at least two comments in your program.
 
+#This if statement helps to compare two or more things together.
+#
+
 left_weights = float(input("What is the weight of the left hand weight?\n"))
 
 right_weights = float(input("What is the weight of the right hand weight?\n"))
