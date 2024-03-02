5c93071e881e46cefb5d2c4ec5974da5e0d0d7f0
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-03-02 16:19:00 +0000

    if statement

---
+++ Selection/Challenges/Challenge43.py
@@ -0,0 +1,3 @@
+#Write a program to input the left - and right hand weights of a rollercoaster ride car. 
+#if the two weights are equal, print "The car is balanced". if the weights are not equal, print 
+#"The car is not balanced." include at least two comments in your program.
