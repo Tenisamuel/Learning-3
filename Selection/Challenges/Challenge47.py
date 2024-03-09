3fa937f4df8ef25412c25608f6acd60553776329
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-03-09 17:52:51 +0000

    Modified

--- Selection/Challenges/Challenge47.py
+++ Selection/Challenges/Challenge47.py
@@ -2,17 +2,19 @@
 #"20 minutes" if the user inputs "Y" , ask the user "is your pulse rate between 60 and 100bpm?"
 
 #If their pulse is between 60 and 100 bpm, print "your heart rate is within the expected range" otherwise print
-#"your resting heart rate is outside of the expected range. you may wish to see a proffesional,"
+#"your resting heart rate is outside of the expected range. you may wish to see a proffesional"
 
 #If the user priints "N". print "Rest for 20 minutes and try again !"
-<<<<<<< HEAD
-=======
 
+first_question = str(input("Have you been at rest for at least 20 minutes"))
 
-rest_time = str(input("Have you been at rest for at least 20 minutes\n"))
+if first_question == "Y":
+    second_question = str(input("Is your heart rate between 60 and 100bpm?"))
 
-if rest_time == "Y":
-  next_Q = str(input("Is your pulse rate between 60 and 100bpm\n"))
-  if next_Q  == "Y":
-    print("your heart rate is within the expected range")
->>>>>>> 885155e (Resr)
+    if second_question == "Y":
+        print("Your heart rate is within the expected range ")
+
+    else:
+        print("your resting heart rate is outside of the expected range. you may wish to see a proffesional")
+elif first_question == "N":
+    print("Rest for 20 minutes and try again !")
