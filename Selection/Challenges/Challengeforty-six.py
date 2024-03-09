be597ee6d17acbff77e972a33e2e4cf698c5ddea
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-03-09 18:12:23 +0000

    question

---
+++ Selection/Challenges/Challengeforty-six.py
@@ -0,0 +1,40 @@
+#Program name: Level 3 Challenge 46 Quiz incomplete
+#This program asks three quiz questions, and scores the answers
+
+#(Correct answers are: hummingbird, False, bullfrog)
+
+#initialize score
+score = 0
+print("Answer each of the following three questions:")
+answer = input("\nWhich is the only bird that can fly backwards? \n\
+(a) Hummingbird, (b) Swift, (c) Flamingo? ")
+
+#Each correct answer scores 1
+
+#correct answer is a
+if answer == "a" or answer == "A":
+    .............................
+
+answer = input("\nYou can get warts from touching a toad - \n\
+True (T) or False (F)? ")
+
+#correct answer is F
+if ................................
+    ...........................
+
+#correct answer is a
+answer = input("\nWhich animal never sleeps? \n\
+(a) bullfrog  (b) whale  (c) butterfly  ")
+....................................
+..............................
+
+print("\nYou scored", ........., "out of 3")
+if score == 3:
+    print("Well done!")
+elif score == 2:
+    print("Pretty good!")
+elif .................
+    print("More nature study required...")
+.....
+
+    print("Oops!")
