2181b564887cd9376b3003b9870df9569a6a078a
Los-merengue <michaels4uandme@gmail.com>
2024-03-23 20:09:13 +0300

    Added the question to challenge48

---
+++ Selection/Challenges/Challenge48.py
@@ -0,0 +1,32 @@
+#Program name: Level 3 Challenge 48 Challenge Fast cash incomplete
+#program simulates selecting "Fast cash" at ATM
+
+print("1. Balance enquiry")
+print("2. Mini statement")
+print("3. Fast cash\n")
+
+#The escape sequence \n causes an extra new, blank line
+
+print("Please select a transaction")
+
+choice = input(..................................
+
+if choice == "1":
+    print("Option 1 selected: balance printed")
+
+elif ..................
+
+    ..................................................
+
+elif ..................
+    print("Option 3 selected: Fast cash" )
+    amount = input("select amount in £: 20, 50 or 100: ")
+    if not ((amount == "20") or (amount == "50") or (.................)):
+        print("Invalid amount")
+
+    ...........
+        print("£" + amount + " dispensed")
+        print("Please take your cash")
+
+...........
+    print("Invalid selection")
