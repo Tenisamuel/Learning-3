3af2a9e7938b41fdacdaffd15b17f38f2325921a
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-03-23 18:05:18 +0000

    Bank withdrawal

--- Selection/Challenges/Challenge49.py
+++ Selection/Challenges/Challenge49.py
@@ -11,9 +11,9 @@ print("Model C (C): 126 dB")
 
 #ask user to input model and recorded decibel test reading
 print("Which model has been tested? ")
-model = input("Enter A for Model A, B for Model B or C for Model C: ")
+model = input("Enter A for Model A, B for Model B or C for Model C:\n")
     
-reading = ............................................   
+reading = float(input("What is the recorded decibel level\n"))  
 
 #test that decibel level is above minimum tolerances
 if reading < 94:
@@ -22,8 +22,8 @@ if reading < 94:
 elif model == "a" or model == "A":    
     print("Passed.") 
 
-elif ...................................
-    if ...................................
+elif model == "b" or model == "B":
+    if reading < 110:
         print("Failed. Requires adjustment or reclassification as Model A.")
     else:
         print("Passed.")
@@ -31,9 +31,9 @@ elif ...................................
 elif model == "c" or model == "C":
     if reading < 126:
         print("Failed. Requires adjustment or reclassification.")
-    .............
+    else:
         print("Passed.")
 
-...........
+else:
     #invalid model entered
     print("Model must be A, B or C. No result given.")
