58f9e62c1bb59ea9cb32d1c4cd918e838aa90fc3
Los-merengue <michaels4uandme@gmail.com>
2024-03-23 20:44:48 +0300

    Added Challenge 49 Siren Problem

---
+++ Selection/Challenges/Challenge49.py
@@ -0,0 +1,39 @@
+#Program name: Level 3 Challenge 49 Siren decibels test incomplete
+#The program determines whether a recorded level of decibels meets the
+#acceptable minimum requirement for each of three models produced in a factory.
+
+#Specify the minimum accepted decibels 
+#on different sirens for quality control tests.
+print("Minimum decibels accepted per model:")
+print("Model A (A): 94 dB")
+print("Model B (B): 110 dB")
+print("Model C (C): 126 dB")
+
+#ask user to input model and recorded decibel test reading
+print("Which model has been tested? ")
+model = input("Enter A for Model A, B for Model B or C for Model C: ")
+    
+reading = ............................................   
+
+#test that decibel level is above minimum tolerances
+if reading < 94:
+    print("The siren has failed the minimum acceptable limit for all models.")    
+
+elif model == "a" or model == "A":    
+    print("Passed.") 
+
+elif ...................................
+    if ...................................
+        print("Failed. Requires adjustment or reclassification as Model A.")
+    else:
+        print("Passed.")
+
+elif model == "c" or model == "C":
+    if reading < 126:
+        print("Failed. Requires adjustment or reclassification.")
+    .............
+        print("Passed.")
+
+...........
+    #invalid model entered
+    print("Model must be A, B or C. No result given.")
