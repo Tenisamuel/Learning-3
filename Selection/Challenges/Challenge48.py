8fd73bf53a94f5dce95f6dd3819144b2b1ddb8e6
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-03-23 17:36:12 +0000

    Bank withdrawal

--- Selection/Challenges/Challenge48.py
+++ Selection/Challenges/Challenge48.py
@@ -9,24 +9,23 @@ print("3. Fast cash\n")
 
 print("Please select a transaction")
 
-choice = input(..................................
+choice = input("Click either 1, 2 or 3")
 
 if choice == "1":
     print("Option 1 selected: balance printed")
 
-elif ..................
+elif choice == "2":
+    print("option 2 selected:mini statement printed")
 
-    ..................................................
-
-elif ..................
+elif choice == "3":
     print("Option 3 selected: Fast cash" )
     amount = input("select amount in £: 20, 50 or 100: ")
-    if not ((amount == "20") or (amount == "50") or (.................)):
+    if not ((amount == "20") or (amount == "50") or (amount == "100"))
         print("Invalid amount")
 
-    ...........
+    else:
         print("£" + amount + " dispensed")
         print("Please take your cash")
 
-...........
+else:
     print("Invalid selection")
