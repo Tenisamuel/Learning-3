d6f5388e1424288d95b232383b4104691660913f
Emeka Michael Nzeopara <michaels4uandme@gmail.com>
2024-04-11 19:50:47 +0300

    Added a question to answer

--- Iteration/Challenge52.py
+++ Iteration/Challenge52.py
@@ -1,4 +1,6 @@
 #Write a program for an arcade game to display "you win!" three times and then print "Congratulations!"
-for i in range(5,15):
-    teni = i * 3
-    print(teni)
+
+for i in range(0,7):
+    teni = print("Teni is good")
+    
+print("Yes he is good")
