ee28798c747a2e1dfa0aeb7a363bdc7eeba8c288
Emeka Michael Nzeopara <michaels4uandme@gmail.com>
2024-04-11 19:57:02 +0300

    Added some practise script

--- Iteration/Challenge52.py
+++ Iteration/Challenge52.py
@@ -1,6 +1,2 @@
 #Write a program for an arcade game to display "you win!" three times and then print "Congratulations!"
 
-for i in range(0,7):
-    teni = print("Teni is good")
-    
-print("Yes he is good")
---
+++ Iteration/Practise-Question.py
@@ -0,0 +1,5 @@
+#Write a for loop that will print from 1 to 10
+#Write a for loop that will print from 4 to 18
+#Write a for loop that will print from 2 to 20
+#Write a for loop that will print from 8 to 16
+
