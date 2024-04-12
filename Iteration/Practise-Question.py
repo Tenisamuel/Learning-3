5d5ba3222dc2b42a92893560ce8037ba3c83167a
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-04-12 12:16:27 +0100

    Fav game

--- Iteration/Practise-Question.py
+++ Iteration/Practise-Question.py
@@ -3,3 +3,14 @@
 #Write a for loop that will print from 2 to 20
 #Write a for loop that will print from 8 to 16
 
+for i in range(1,11):
+    print(i)
+
+for i in range(4,19):
+    print(i)
+
+for i in range(2,21):
+    print(i)
+
+for i in range(8,17):
+    print(i)
