9bb8d8e88e8142a380f1dbf9e21724d61dabb80c
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-04-13 18:15:50 +0100

    saved

--- Iteration/Challenge53.py
+++ Iteration/Challenge53.py
@@ -1,8 +1,10 @@
 #Write a program to count down from 10 to 0, displaying each value on a seperate line.
 
+
 for x in range(10,-1,-1):
     print(x)
 
 #Write a program that countdown from 15 to 2
 #Write a program that countdown from 25 to 5
 #Write a program that countdown from 27 to 7
+
