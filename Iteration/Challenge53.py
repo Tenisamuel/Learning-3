351345d8c230f28fee46e667e276351847ab8aba
Emeka Michael Nzeopara <michaels4uandme@gmail.com>
2024-04-13 20:15:33 +0300

    Added some script

--- Iteration/Challenge53.py
+++ Iteration/Challenge53.py
@@ -1,7 +1,7 @@
 #Write a program to count down from 10 to 0, displaying each value on a seperate line.
 
 for x in range(10,-1,-1):
-    print(i)
+    print(x)
 
 #Write a program that countdown from 15 to 2
 #Write a program that countdown from 25 to 5
