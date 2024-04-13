7430e9078a0ab2d9755d425abb34965f75db6b42
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-04-13 17:01:26 +0100

    Result

---
+++ Iteration/Challenge53.py
@@ -0,0 +1,8 @@
+#write a program for an arcade game to display "You win!" three times and print congratulations.
+
+for i in range(1,4):
+    print((str(i)) +  " You win")
+
+print("Congratulations!")
+
+
