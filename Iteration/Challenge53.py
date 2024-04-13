72bd6a7407847d280d892345252699ddd086ac1d
Emeka Michael Nzeopara <michaels4uandme@gmail.com>
2024-04-13 20:06:02 +0300

    Modify the program for other examples

--- Iteration/Challenge53.py
+++ Iteration/Challenge53.py
@@ -1,4 +1,8 @@
 #Write a program to count down from 10 to 0, displaying each value on a seperate line.
 
-for x in range(0,6,-1):
+for x in range(10,-1,-1):
     print(x)
+
+#Write a program that counts from 15 to 2
+#Write a program that counts from 20 to 5
+#Write a program that counts from 27 to 8
