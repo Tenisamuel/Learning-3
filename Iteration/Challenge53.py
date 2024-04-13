10f00badc468492f8fc946321e9f86bf670fdf4d
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-04-13 18:35:44 +0100

    Answered

--- Iteration/Challenge53.py
+++ Iteration/Challenge53.py
@@ -1,10 +1,18 @@
 #Write a program to count down from 10 to 0, displaying each value on a seperate line.
 
 
-for x in range(10,-1,-1):
-    print(x)
+#for x in range(10,-1,-1):
+#    print(x)
 
 #Write a program that countdown from 15 to 2
 #Write a program that countdown from 25 to 5
 #Write a program that countdown from 27 to 7
 
+#for x in range(15,1,-1):
+ #   print(x)
+
+#for x in range(25,4,-1):
+#    print(x)
+
+#for x in range(27,6,-1):
+#    print(x)
