697bf6a304d77999f42e63827a7592eef023697b
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-04-20 17:45:08 +0100

    Commited

---
+++ Iteration/Challenge54.py
@@ -0,0 +1,5 @@
+#write a program to ask a user which times table they would like to see displayed.
+#Display the times table from 2 to 12.
+
+
+Timetables = input("Which times table would you like to be displayed")
