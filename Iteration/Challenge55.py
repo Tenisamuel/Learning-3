0fd7152c4c1ff73f2f9bdfcd0fe7ce8ccdc23ed5
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-04-20 18:59:18 +0100

    Updated version of Challenge55

---
+++ Iteration/Challenge55.py
@@ -0,0 +1,8 @@
+#write a program to ask the user to input two positive integers a and b where a < b. print all the numbers
+#between a and B inclusive that are divisible by 13.
+
+a = int(input("Write one positive integer"))
+
+b = int(input("Write one positive integer"))
+
+for i in range()
