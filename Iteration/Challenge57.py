1003ab4121ad577a9f7b84084b5644cc9719b4ba
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-04-24 19:06:17 +0100

    Updated version Challenge 57.py

---
+++ Iteration/Challenge57.py
@@ -0,0 +1,9 @@
+#Write a program to count numbers from 0 to 100 in 5s snd print them on one line.
+
+backwards = [i for i in range (0,105,5)]
+print(backwards)
+
+#  OR
+
+for i in range (0,105,5):
+    print(i, end = " ")
