1ba46754a678d447953848841c4e832df57f097a
Emeka Michael Nzeopara <michaels4uandme@gmail.com>
2024-04-27 18:02:17 +0300

    Modify the code to have the divisible aspect

--- Iteration/Challenge56.py
+++ Iteration/Challenge56.py
@@ -11,6 +11,7 @@ print("\n")
 
 if (type_first_positive_integer_a < type_second_positive_integer_b):
     for i in range(type_first_positive_integer_a,type_second_positive_integer_b + 1):
+        #If the remainder of a dividend is zero, i.e that the dividend is divisible by the divisor.
         if (i % 13 == 0):
             print(i)
 
