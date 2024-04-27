1c1923353dc5169d944f5a990dbc9afe1c6b3718
Emeka Michael Nzeopara <michaels4uandme@gmail.com>
2024-04-27 18:53:25 +0300

    Added the solution to the code

--- Iteration/Challenge58.py
+++ Iteration/Challenge58.py
@@ -1,2 +1,14 @@
 #write a program that asks an employee to enter thier overtime hours each day of the week to the nearest
-#quater hour, e.g, 1.25. The total overtime over the week is caculated and printed at the end of the program
+#quater hour, e.g, 1.25. The total overtime over the week is caculated and printed at the end of the 
+#program
+
+print("overtime calculator\n")
+print("Enter your overtime hours for Monday-Friday (day 1 to day 5)\
+\nto the nearest quarter hour, e.g. 1.25\n")
+total = 0
+
+for day in range(1,6):
+    hours = float(input("Enter overtime worked on day " + str(day) + " in hours: "))
+    total = total + hours
+
+print("\nYou worked",total,"overtime hours this week.")
