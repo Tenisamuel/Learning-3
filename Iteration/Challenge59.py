0639e4a5d284219ee705b44f1ee6a03b7c3c9f4c
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-04-27 15:04:18 +0100

    Update

---
+++ Iteration/Challenge59.py
@@ -0,0 +1,6 @@
+#write a program that asks the user to enter 5 numbers with decimal points between 1.0 and
+#and 10.0. print out how many of the numbers entered are whole numbers
+
+Enter = float(input("Write 5 numbers with decimal points\n"))
+
+if Enter = int(input("Write 5 numbers with decimal points\n"))
