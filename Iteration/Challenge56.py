e589843fb314122e78febd72ea44d32a260ebbf7
Emeka Michael Nzeopara <michaels4uandme@gmail.com>
2024-04-27 17:32:25 +0300

    Updated challenge56.py with solution

--- Iteration/Challenge56.py
+++ Iteration/Challenge56.py
@@ -1,2 +1,15 @@
 #Write a test plan for challenge 55 to include test data for different user entries, 
 #together when expected results. Test your program
+
+type_first_positive_integer_a = int(input("Write the first positive integer:\n"))
+
+type_second_positive_integer_b= int(input("Write the second positive integer:\n"))
+
+print("\n")
+
+if (type_first_positive_integer_a < type_second_positive_integer_b):
+    for i in range(type_first_positive_integer_a,type_second_positive_integer_b + 1):
+        print(type_first_positive_integer_a, type_second_positive_integer_b)
+
+else:
+    print("The value of a is greater than b, you need to satisfy the condition")
