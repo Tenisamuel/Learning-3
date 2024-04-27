90bc3e84cbac1c2a2f824d7bb5fdd5f688927dde
Emeka Michael Nzeopara <michaels4uandme@gmail.com>
2024-04-27 18:09:53 +0300

    Added a Practise Question to Try

---
+++ Iteration/Practise-Question2.py
@@ -0,0 +1,3 @@
+#write a program to ask the user to input two positive integers x and y where y > x. print all the numbers
+#between x and y inclusive that are divisible by 9. Also write a test to make sure that this condition
+#is satisfied
