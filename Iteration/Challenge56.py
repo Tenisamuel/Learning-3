90c3ba19335d77bcba105b4910af09e14a4ad285
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-04-27 15:23:11 +0100

    update

--- Iteration/Challenge56.py
+++ Iteration/Challenge56.py
@@ -1,2 +1,2 @@
-#Write a test plan for challemge 55 to include test data for different user entries, 
+#Write a test plan for challenge 55 to include test data for different user entries, 
 #together when expected results. Test your program
