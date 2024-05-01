9abe4a06cdd810a648b1f62bc7067e50ab8e3236
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-05-01 17:37:58 +0100

    Updated Challenge 60

--- Iteration/Challenge59.py
+++ Iteration/Challenge59.py
@@ -2,4 +2,3 @@
 #and 10.0. print out how many of the numbers entered are whole numbers
 
 Enter = float(input("Write 5 numbers with decimal points\n"))
-
---
+++ Iteration/Challenge60.py
@@ -0,0 +1,9 @@
+#Write a program to display the times tables 7 to 10, displaying each table in format 7 x 1 = 7 etc.
+
+Timestable_two = int(input("Write the following timestables you want to b displayed 7,8,9 or 10"))
+
+
+for i in range(1,13):
+    result = Timestable_two * i
+    print(Timestable_two, "X", i, " =", result)
+    
