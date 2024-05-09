f253c52db6404d81734a144b9219020fbab0b88c
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-05-09 18:03:04 +0100

    Updated Version Challenge 61

---
+++ Iteration/Challenge61.py
@@ -0,0 +1,7 @@
+#A store sells a brand of italian bathroom tiles in different lengths and widths.Tiles come in length
+#of 10, 15, 20cm and in widths of 10 and 12cm, or in lengths of 4, 6 an 8 inches and in widths of 4 and 5 inches.
+#Customers can select which units to use.
+
+#Write a program to ask the user to choose their preferred units of measurement and then calculate and print,
+#for each length and each width, the area covered 1 tile
+
