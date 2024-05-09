0b0f5b7a89e8f61652869150de40dc78867cbf15
Emeka Michael Nzeopara <michaels4uandme@gmail.com>
2024-05-09 19:26:12 +0300

    Added the challenge59.py solution

--- Iteration/Challenge59.py
+++ Iteration/Challenge59.py
@@ -1,4 +1,14 @@
 #write a program that asks the user to enter 5 numbers with decimal points between 1.0 and
 #and 10.0. print out how many of the numbers entered are whole numbers
 
-Enter = float(input("Write 5 numbers with decimal points\n"))
+
+print("Enter five numbers with or without decimal points")
+print("The program prints how many were whole numbers\n")
+
+total = 0
+
+for n in range(5):
+    num = float(input("Enter number: "))
+    if num == int(num):
+        total = total + 1
+print("You entered",total,"whole numbers")
