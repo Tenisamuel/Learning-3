01a8e99be8010543b13b6f92455134a50c5250e3
Emeka Michael Nzeopara <michaels4uandme@gmail.com>
2024-05-09 20:26:07 +0300

    Solution to the challenge61.py

--- Iteration/Challenge61.py
+++ Iteration/Challenge61.py
@@ -5,3 +5,22 @@
 #Write a program to ask the user to choose their preferred units of measurement and then calculate and print,
 #for each length and each width, the area covered 1 tile
 
+units = input("Enter the units of the area you want to calculate: Answer C or I \n")
+
+if units == "C" or units == "c":
+    print("Tiles Calculations will be done in cm\n")
+    for length in range(10,21,5):
+        for width in range(10,13,2):
+            area = length * width
+            print("Length", length,"cm \n Width", width, "cm \n Area", area, "cm\n\n")
+
+elif units == "I" or units == "i":
+    print("Tiles Calculations will be done in inches")
+    for length in range(4,9,2):
+        for width in range(4,6):
+            area = length * width
+            print("Length", length,"inches \n Width", width, "inches \n Area", area, "inches\n\n")
+
+else:
+    print("You did not enter the answer that is required for your program")
+            
