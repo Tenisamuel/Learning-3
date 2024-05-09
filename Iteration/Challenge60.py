016f11bca2c2e737c764df414377aa84ddf83e3a
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-05-09 17:19:23 +0100

    Updated Challenge 60 Version 3

--- Iteration/Challenge60.py
+++ Iteration/Challenge60.py
@@ -1,6 +1,6 @@
 #Write a program to display the times tables 7 to 10, displaying each table in format 7 x 1 = 7 etc.
 
-Timestable_two = int(input("Write the following timestables you want to b displayed 7,8,9 or 10"))
+Timestable_two = int(input("Write the following timestables you want to b displayed 7,8,9 or 10\n"))
 
 if (Timestable_two >= 7) and (Timestable_two <= 10):
     for i in range(1,13):
