be5332796c0e0715f7f063ccfab0f2d4c858cb73
Emeka Michael Nzeopara <michaels4uandme@gmail.com>
2024-05-11 20:08:48 +0300

    Added the program attached to Challenge63.py

--- Iteration/Challenge63.py
+++ Iteration/Challenge63.py
@@ -1,2 +1,14 @@
 #Enter the program in Example 6, using the same test data.Use the round() function to round averageLengths
 #to 1 decimal place Save and execute your program.
+
+day = 0
+totalLengths = 0
+lengths = int(input("Enter number of lengths"))
+
+while lengths != -1:
+    day = day + 1
+    totalLengths = totalLengths + lengths
+    lengths = int(input("Enter number of lengths, -1 to end: "))
+
+averageLengths = totalLengths / day
+print("Average daily number of lengths:", averageLengths)
