8cdfc762a572ddb17109eae83a9e065f2e83d174
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-05-11 17:12:10 +0100

    Updated version

---
+++ Iteration/Challenge62.py
@@ -0,0 +1,3 @@
+#Write a program to ask the user to enter the number of lengths swum by jane on a certain day if the number of
+#lengths is not between 10 and 150,ask the user to re enter until a valid length is entered.print a short
+#message "Data accepted" when valid data is entered.
