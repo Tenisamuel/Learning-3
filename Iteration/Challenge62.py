ad27fbbf69d02a9260d6e2bdbac67743ac6d4114
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-05-11 17:44:07 +0100

    Updated version 2

--- Iteration/Challenge62.py
+++ Iteration/Challenge62.py
@@ -2,10 +2,10 @@
 #lengths is not between 10 and 150,ask the user to re enter until a valid length is entered.print a short
 #message "Data accepted" when valid data is entered.
 
-length = int(input("Enter the Length that is assumed to be swum by Jane: \n"))
+lengths = int(input("Write a number asumed to be the length of jane if she swam between the lengths 10 and 150\n"))
 
-while length not in range(10,151):
-    print("You enter the wrong value, You need to satisfy the condition of the length swum by Jane\n\n")
-    length = int(input("Re-Enter the length swum by Jane\n"))
+while lengths not in range (10,151):
+    print("Re-enter a value between 10 and 150 you must satisfy the condition")
+    lengths = int(input("Write a number asumed to be the length if jane between 10 and 150\n"))
 
-print("Data Accepted")
+print("Good answer")
