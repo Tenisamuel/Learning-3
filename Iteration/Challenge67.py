df1f779561ae85829d5b3a47df7d8cb9ff215b09
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-05-18 19:03:38 +0100

    Updated Challenge 67.py

---
+++ Iteration/Challenge67.py
@@ -0,0 +1,4 @@
+#The program asks the user to enter a number between 1 and 10. if the number is not in thi range, keep printing
+#a warning message and asking the user to re enter the number. Then keep doubling this number until the result
+# 100,000 or more.=print the final result and the number of times the number was doubled.
+
