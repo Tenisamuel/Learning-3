b244b7d7813e55a479dc59d0a5d0701f5c602779
Emeka Michael Nzeopara <michaels4uandme@gmail.com>
2024-05-18 19:41:16 +0300

    Another solution to challenge 65

---
+++ Iteration/Challenge65b.py
@@ -0,0 +1,16 @@
+#Program name Challenge 65 Average runner time
+
+numberOfRrunners = 0
+totalTime = 0
+moreData = True
+print("Enter -1 when no more data")
+while moreData:
+    runnerName = input("Enter name of runner, xxx to end: ")
+    if runnerName != "xxx":
+        raceTime = float(input("Enter time for " + runnerName +": "))
+        numberOfRrunners = numberOfRrunners + 1
+        totalTime = totalTime + raceTime
+    else:
+        moreData = False
+averageTime = totalTime/numberOfRrunners
+print("Average race time in seconds:", round(averageTime,2))
