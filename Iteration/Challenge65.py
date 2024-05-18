bf7f79d4e695ecd268e81701d8a0d67e7911cab2
Emeka Michael Nzeopara <michaels4uandme@gmail.com>
2024-05-18 19:52:37 +0300

    Created data for the challenge65b task

--- Iteration/Challenge65.py
+++ Iteration/Challenge65.py
@@ -18,3 +18,7 @@ while timeofrunners != -1:
     averagetimes = totaltimeofrunners / times
     print("The average time in seconds",averagetimes)
 
+# Teni : 36.5 seconds
+# Samuel : 38.2 seconds
+# Emeka : 39 seconds
+# Uche : 41.2 seconds
