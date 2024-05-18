74c81735822d6b7968cf20a8bb3e4eb84de89ff5
Emeka Michael Nzeopara <michaels4uandme@gmail.com>
2024-05-18 21:05:07 +0300

    Added solution Challeng6.py

--- Iteration/Challenge67.py
+++ Iteration/Challenge67.py
@@ -2,3 +2,25 @@
 #a warning message and asking the user to re enter the number. Then keep doubling this number until the result
 # 100,000 or more.=print the final result and the number of times the number was doubled.
 
+validNum = False
+while not validNum:
+    num = int(input("Enter a number between 1 and 10: "))
+    if num >= 1 and num <= 10:
+        validNum = True
+        print("The number will be doubled repeatedly until the result >= 100,000")
+    else:
+        validNum = False
+        print ("\nInvalid number!")
+
+#initialise count
+
+count = 0
+
+
+#double the input number until num >= 100,000
+while num < 100000:
+    num = num * 2
+    count = count + 1
+
+#print the result and how many times the number was doubled
+print("Final number:",num, " Number of times doubled:",count)
