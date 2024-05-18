8322a92df160c78856944aa4af1ba1c8c8314386
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-05-18 18:16:59 +0100

    Updated Challenge 66

---
+++ Iteration/Challenge66.py
@@ -0,0 +1,9 @@
+#The program asks the user to enter the size in m square of an area that needs tiling. The user then selects
+#The size of tiles they want from a range of sizes given in cm. The program tells them how many tiles 
+#they would need (Ten percent is added to the caculated number to allow for tiles that need to be cut, and
+#this number is rounded down to the nearest whole number)
+
+#Copy and complete the program and save it in your own folder.
+
+#Test your completed program by entering 20cm for tile length, 12cm for tile width and 2.4m square for area to
+#covered.The integer number of tiles required, including the 10% extra, is 110. 
