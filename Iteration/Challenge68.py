ee574c455f928d3cd50e99ce67b4de9ac1682259
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-05-21 21:03:10 +0100

    Updated Challenge 68

---
+++ Iteration/Challenge68.py
@@ -0,0 +1,3 @@
+#Amend the program Challenge 68 Double a number repeatedly completed so that the user canenter the final target.
+#include validation to ensure the target entered by the user is between 20 and 100,000. Also include a print 
+#statement so that the number, and number of time is doubled,is printed each time.
