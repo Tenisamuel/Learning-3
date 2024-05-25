2652cefdf571eee24bf862f5faee702741866ee6
Los-merengue <michaels4uandme@gmail.com>
2024-05-25 20:22:11 +0300

    Added some example to the question

--- Selection/Example/example3-iterations.py
+++ Selection/Example/example3-iterations.py
@@ -1,2 +1,10 @@
 # Write a Python program that asks the user to enter a number and then prints the sum of all numbers from 1 to that number. 
 # The program should use a while loop to calculate the sum.
+
+# Example
+# Enter a positive integer: 5
+# The sum of numbers from 1 to 5 is 15
+
+# In this example, the sum of numbers from 1 to 5 is 
+# 1 + 2 + 3 + 4 + 5 = 15
+# 1+2+3+4+5=15.
