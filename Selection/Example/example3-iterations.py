7e51284a48a389c9c0a6479cb2ad5d9d77aee4df
Los-merengue <michaels4uandme@gmail.com>
2024-05-25 20:04:56 +0300

    Added a question to the exercise

---
+++ Selection/Example/example3-iterations.py
@@ -0,0 +1,2 @@
+# Write a Python program that asks the user to enter a number and then prints the sum of all numbers from 1 to that number. 
+# The program should use a while loop to calculate the sum.
