fcfea9e62b22c1a8c5164c9f3d8cc2f8ec2d6b4e
Los-merengue <michaels4uandme@gmail.com>
2024-05-25 19:39:48 +0300

    Solution to the challenge68

--- Iteration/Challenge68.py
+++ Iteration/Challenge68.py
@@ -2,3 +2,37 @@
 #include validation to ensure the target entered by the user is between 20 and 100,000. Also include a print 
 #statement so that the number, and number of time is doubled,is printed each time.
 
+#validate the user entry of initial starting number
+validNum = False
+while not validNum:
+    num = int(input("Enter a number between 1 and 10: "))
+    if num >= 1 and num <= 10:
+        validNum = True
+    else:
+        validNum = False
+        print("\nInvalid number!")
+
+
+#validate the user entry of final target
+validTarget = False
+while not validTarget:
+    target = int(input("Enter a final target between 20 and 100000: "))
+    if target >= 20 and target <= 100000:
+        validTarget = True
+    else:
+        validTarget = False
+        print("\nInvalid number!")
+
+#initialise count
+
+count = 0
+
+
+#double the input number until num >= 100000
+while num < target:
+    num = num * 2
+    count = count + 1
+    print(count, num)
+
+#print the result and how many times the number was doubled
+print("Final number:",num, " Number of times doubled:",count)
