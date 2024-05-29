932c3ef833559e17e9a3cbe901b7b7c4875c8821
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-05-29 21:51:34 +0100

    Updated version 2

---
+++ Iteration/Challenge70.py
@@ -0,0 +1,5 @@
+#Wrrite a program to:
+    # allow the user to enter the names and years of five olympic games cites.
+    #caculate and print the names and years of the two cites with the most recent and earliest years
+
+
