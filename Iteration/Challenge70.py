d65b8828c2fa8c7fab130a9dc7e1b8e23d788621
Emeka Michael Nzeopara <michaels4uandme@gmail.com>
2024-05-30 00:32:15 +0300

    Updated the script

--- Iteration/Challenge70.py
+++ Iteration/Challenge70.py
@@ -7,14 +7,16 @@ min_year = 10**8
 max_city = ""
 min_city = ""
 
+teni = 5
+
 for i in range(5):
-    city = str(input("Enter 5 cities in the olympic games"))
-    year = int(input("Enter the 5 years of the cities you have picked in the olympic cites"))
+    city = str(input("Enter 5 cities in the olympic games: \n"))
+    year = int(input("Enter the 5 years of the cities you have picked in the olympic cites: \n"))
     if year > max_year:
         max_city = city
         max_year = year
     if year < min_year:
         min_city = city
         min_year = year
-        
-
+    
+print("The City with the most recent olympic year is", max_city)
