b62de6bc7a2b1655cfdda9164f95c1c1059189d4
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-06-01 18:33:50 +0100

    Updated quiz

--- Arrays and Lists/Quiz.py
+++ Arrays and Lists/Quiz.py
@@ -2,5 +2,16 @@
 #Tuple = ()
 #Dictionary = {}
 
-name = ["teni", "sharon", "tunde", "emeka", "michael", "divine", "abraham", "onyedikachi"]
+#name = ["teni", "sharon", "tunde", "emeka", "michael", "divine", "abraham", "onyedikachi"]
 
+firstname = ["Samuel", "Jason", "Ike", "Jadon", "esther", "Paul", "aliyah", "Mosh"]
+surname =  ["Ola", "Ibe", "Eli", "Fashola", "Gift", "Cicak", "tunde", "Ilyas"]
+
+print(firstname[0], surname[1])
+print(firstname[1], surname[3])
+print(firstname[2], surname[4])
+print(firstname[4], surname[2])
+print(firstname[3], surname[5])
+print(firstname[5], surname[6])
+print(firstname[6], surname[7])
+print(firstname[7], surname[0])
