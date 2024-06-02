fa6ca2a30abb99d9e24827e5cc7b407165bef3b9
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-06-02 15:34:59 +0100

    Created file

---
+++ Arrays and Lists/Challenge72.py
@@ -0,0 +1,8 @@
+#Write a program  to initialise the lise of planets.
+
+#planets = ["Mecury", "Uranus", "Venus", "Neptune"," Mars", "Saturn", "Jupiter", "Earth"]
+
+#Use the list.append() method to append Uranus and Neptune to the end of list 
+#print the list in the format 
+
+["Mecury", "Uranus", "Venus", "Neptune"," Mars", "Saturn", "Jupiter", "Earth"]
