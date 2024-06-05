e73eec061b44c32cd053903c34bac223b349c1f6
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-06-05 20:37:05 +0100

    Updated Challenge 74

---
+++ Arrays and Lists/Challenge74.py
@@ -0,0 +1,4 @@
+#Write a program to initialise and print the list of planets:
+#planet = ["Mecury", "Venus,", "Earth", "Mars", "Jupiter", "Saturn"]
+
+#Then print the list with each planet on a seperate line starting with saturn, and ending with mecury.
