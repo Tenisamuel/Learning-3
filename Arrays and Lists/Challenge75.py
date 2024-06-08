f9737034884aee619b1c62199563682bef9680c1
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-06-08 18:19:58 +0100

    Updated Challenge 75.py

---
+++ Arrays and Lists/Challenge75.py
@@ -0,0 +1,12 @@
+#Write a program to ask the user to enter the number of astronauts sent on space missions
+#in the years 2016 to 2020 e.g "How many astronauts on space missions in 2016?" 
+
+#store the number in a list named astronauts
+
+#Print the list list and the total number of astronauts sent on space missions from 2016 to 2020
+
+num_of_astronauts = []
+prompt = input("Write a list of astronauts in space missions in 2016")
+
+num_of_astronauts.append(prompt)
+print("The number of astronauts that went to space is",)
