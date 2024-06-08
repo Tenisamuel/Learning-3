670d8ecb577ef8a84ea5aacd41c085f732df8de6
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-06-08 16:51:56 +0100

    Updated Challenge 74!

--- Arrays and Lists/Challenge74.py
+++ Arrays and Lists/Challenge74.py
@@ -2,3 +2,7 @@
 #planet = ["Mecury", "Venus,", "Earth", "Mars", "Jupiter", "Saturn"]
 
 #Then print the list with each planet on a seperate line starting with saturn, and ending with mecury.
+
+planets_on_next_line = ["Mecury", "Venus", "Earth", "Mars", "Jupiter", "Saturn"]
+
+print(*planets_on_next_line, sep="\n")
