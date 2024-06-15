251e1e27333fa46d36fcf41b250da70693f7a1ab
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-06-15 17:35:01 +0100

    Updated all

--- GettingsStarted/Teni.py
+++ GettingsStarted/Teni.py
@@ -1,6 +1,1 @@
-print("The guide told us Stay very still!")
-print("Gorilla's are the largest living primates")
-print("The scientific name for the Western Lowland\ngorilla is Gorilla gorilla gorilla")
 
-largestprimates = "largestlivingprimates are gorillas"
-print(largestprimates)
