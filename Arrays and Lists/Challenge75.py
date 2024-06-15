c20d5b786dd848524b3da04ba2684a5bbdae580c
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-06-15 17:28:08 +0100

    Updated Challenge 75

--- Arrays and Lists/Challenge75.py
+++ Arrays and Lists/Challenge75.py
@@ -8,7 +8,7 @@
 astronauts = []
 
 for i in range(0,12):
-    prompt = input("Write down a list of astronauts that were in space in 2016")
+    prompt = input("Write down a list of astronauts that were in space in 2016\n")
     astronauts.append(prompt)
 
 print("The following were the names of astronauts that were on space missions in 2016:", astronauts)
