27757b670601230a3ba941679206354ce29d0614
Emeka Michael Nzeopara <michaels4uandme@gmail.com>
2024-06-15 19:59:28 +0300

    Added the List planet program with errors

---
+++ Arrays and Lists/Challenge77.py
@@ -0,0 +1,16 @@
+planetList = ()
+moonList = ()
+moreData = True
+print("\nPlanets and moons\n")
+
+while not moreData:
+    planetName = input("Enter name of planet, xxx to finish: ")
+    if planetName = "xxx":
+        moreData = False
+    else:
+        numMoons = int(input("Enter number of moons: ")
+        append.planetList(planetName)
+        append.moonList(numMoons)
+
+for index in range(len(planetList)):
+    print(planetList[index] + " has " + str(moonList[index]) + " moons")
