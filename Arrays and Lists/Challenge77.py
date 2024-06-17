9955b74444e0ab3f4c971d993f1f33c20f2bf806
Emeka Michael Nzeopara <michaels4uandme@gmail.com>
2024-06-17 20:12:05 +0300

    Restarting the question

--- Arrays and Lists/Challenge77.py
+++ Arrays and Lists/Challenge77.py
@@ -1,16 +1,18 @@
-planetlist = []
-moonList = []
+#Program name: Level 5 Challenge 77 Lists planet and moons with errors
+
+planetList = ()
+moonList = ()
 moreData = True
 print("\nPlanets and moons\n")
 
-while moreData:
+while not moreData:
     planetName = input("Enter name of planet, xxx to finish: ")
-    if planetName == "xxx":
+    if planetName = "xxx":
         moreData = False
     else:
-        numMoons = int(input("Enter number of moons: "))
-        planetlist.append(planetlist)
-        moonList.append(numMoons)
+        numMoons = int(input("Enter number of moons: ")
+        append.planetList(planetName)
+        append.moonList(numMoons)
 
-for index in range(len(planetlist)):
-    print(planetlist[index], "has" , (moonList[index]) , " moons")
+for index in range(len(planetList)):
+    print(planetList[index] + " has " + str(moonList[index]) + " moons")
