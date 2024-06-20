a70c2d8b10a5ba7cfc9c19577fa23505baa4cf7e
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-06-20 20:49:15 +0100

    Updated all

---
+++ Arrays and Lists/Challenge78.py
@@ -0,0 +1,14 @@
+#Create two lists.One list contains the names of eight plantes.The second list Contains the sizes of the planets
+#in percentages relative them.
+#Jupiter 1120% the size of Earth.
+#Saturn 945% the the size of Earth.
+#Uranus 400% the size of Earth.
+#Neptune 390% the size of Earth.
+#Venus 95% the size of Earth.
+#Mars 53% the size of Earth.
+#Mecury 38% the size of Earth.
+#Earth 100% the size of Earth.
+
+#From the two lists, print the information in the format given above.
+
+nameofplanets = ["Jupiter","Saturn","Uranus","Neptune","Venus","Mars","Mecury","Earth"]
