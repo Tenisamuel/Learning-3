b315e7247baadb297cf07d69837ae77f5052143e
Emeka Michael Nzeopara <michaels4uandme@gmail.com>
2024-06-22 02:44:52 +0300

    Updated the script

--- Arrays and Lists/Challenge77.py
+++ Arrays and Lists/Challenge77.py
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 #Program name: Level 5 Challenge 77 Lists planet and moons with errors
 
 planetList = []
-=======
-planetlist = []
->>>>>>> a70c2d8b10a5ba7cfc9c19577fa23505baa4cf7e
 moonList = []
 moreData = True
 print("\nPlanets and moons\n")
@@ -15,11 +11,7 @@ while moreData:
         moreData = False
     else:
         numMoons = int(input("Enter number of moons: "))
-<<<<<<< HEAD
         planetList.append(planetName)
-=======
-        planetlist.append(planetlist)
->>>>>>> a70c2d8b10a5ba7cfc9c19577fa23505baa4cf7e
         moonList.append(numMoons)
 
 for index in range(len(planetlist)):
