76a923bf04e9ba22c5b52353fcd160614c4af41b
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-06-29 10:59:20 +0100

    Updated challenge 81

--- Arrays and Lists/Challenge81.py
+++ Arrays and Lists/Challenge81.py
@@ -1,5 +1,8 @@
 #Write a program which prints the names of the largest and the smallest planets in the lists described.
+descending_order = ["Jupiter 1120% the size of Earth. " "Saturn 945% the the size of Earth.""Uranus 400% the size of Earth.""Venus 95% the size of Earth.""Mars 53% the size of Earth."]
 
-descending_orderofplanets = ["Jupiter 1120% the size of Earth. " "Saturn 945% the the size of Earth.""Uranus 400% the size of Earth.""Venus 95% the size of Earth.""Mars 53% the size of Earth."]
 
-print(descending_orderofplanets, sep ="\n")
+for i in range(len(descending_order)):
+    print(descending_order[i])
+
+
