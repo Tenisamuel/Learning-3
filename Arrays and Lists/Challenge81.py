bb51681b33ba5c9231d21a82c6385c8ddccab6fc
Emeka Michael Nzeopara <michaels4uandme@gmail.com>
2024-06-29 13:00:06 +0300

    Solution to script

--- Arrays and Lists/Challenge81.py
+++ Arrays and Lists/Challenge81.py
@@ -1,8 +1,29 @@
 #Write a program which prints the names of the largest and the smallest planets in the lists described.
-descending_order = ["Jupiter 1120% the size of Earth. " "Saturn 945% the the size of Earth.""Uranus 400% the size of Earth.""Venus 95% the size of Earth.""Mars 53% the size of Earth."]
+#Program name: Challenge 81 largest and smallest planets
+planet = ["Jupiter","Saturn","Uranus",\
+          "Neptune","Venus","Mars", "Mercury","Earth"]
+size = [1120,945,400,390,95,53,38,100]
 
 
-for i in range(len(descending_order)):
-    print(descending_order[i])
+#initialise variables
+numberOfPlanets = len(planet)
+
+#initialise max and min values to size of first item in each list
+minSize = size[0]
+minPlanet = planet[0]
+maxSize = size[0]
+maxPlanet= planet[0]
+
+#check size of each planet except the first planet
+for n in range(1,numberOfPlanets):
+    if size[n] > maxSize:
+        maxSize = size[n]
+        maxPlanet = planet[n]
+    if size[n] < minSize:
+        minSize = size[n]
+        minPlanet = planet[n]
+
+print("Largest planet: "+str(maxPlanet)+"'s radius "+str(maxSize)+"% of earth's radius")
+print("Smallest planet: "+str(minPlanet)+"'s radius "+str(minSize)+"% of earth's radius")
 
 
