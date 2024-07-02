d534eabc9201696f060f23745cfe2b2546cf2944
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-07-02 20:56:34 +0100

    Updated Challenge 82

---
+++ Arrays and Lists/Challenge82.py
@@ -0,0 +1,8 @@
+#In Example 2. The figure of newcastle in july has been incorrectly entered .write a statement to amendthe figure
+#in the two-dimensional list booking to 96.
+
+newcastle_campervans = [43,52,92,101]
+
+for i in range(len(newcastle_campervans)):
+    newcastle_campervans[2] = 96
+    print(newcastle_campervans)
