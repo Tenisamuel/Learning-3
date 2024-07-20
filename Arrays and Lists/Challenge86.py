389c2f467f9f9718af5a597c4363bee7e14236c1
Emeka Michael Nzeopara <michaels4uandme@gmail.com>
2024-07-20 20:41:49 +0300

    Created Challeng86.py Assignment

---
+++ Arrays and Lists/Challenge86.py
@@ -0,0 +1,24 @@
+#Program name: Level 5 Challenge 86 Lion cubs incomplete
+#Program calculates total number of lion cubs born to three lionesses
+#during 2019 and 2020
+
+#name is a list of 3 elements, initialised with the names of the three lions
+#the first name is referred to as name[0], the second name as name[1], etc
+
+name = ["Tatiana","Sabrina","Luna"]
+
+#initialise integer variable totalCubs to zero
+......................
+
+#ask user to input data for two years, 2019 and 2020
+for year in range (2019,.....):
+    #print a blank line
+    print("\n")
+    for lioness in ..............:
+        cubs = int(input("Enter number of cubs born to " + name[lioness]\
+                         + " in " + str(year) + ": "))
+
+        #add to cumulative total
+        .............................
+
+print("Total cubs born:"...................
