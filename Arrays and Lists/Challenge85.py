450b29ddd25de932f9e86d872159eadad1795534
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-07-20 17:56:32 +0100

    Updated Challenge 85

--- Arrays and Lists/Challenge85.py
+++ Arrays and Lists/Challenge85.py
@@ -13,21 +13,19 @@ score = [ [75, 59, 63],
 #name = ["Amber", "Cindy", "Valentina", "Isabella", "Terri"]
 
 #initialise list of five totals
-#total = [sum(score[0]), sum(score[1]), sum(score[2]), sum(score[3]), sum(score[4]) ]
-
-total = [0]*5
+total = [sum(score[0]), sum(score[1]), sum(score[2]), sum(score[3]), sum(score[4]) ]
 
 #initialise maxScore
-maxScore = 0
+maxScore = 
 
-for skater in range(5):
-    for judge in range(3):
-        total[skater] = total[skater] + score[skater][judge]
-    if total[skater] > maxScore:
-        maxScore = total[skater]
-        winnerName = name[skater]
+#for skater ........................
+#    for judge .....................
+#       total[skater] = total[skater] + score[.........][............]
+#    if total[skater] > maxScore:
+#       ..............................
+#       winnerName = .....................
 
-print("Winner:", winnerName, " Score:", maxScore)
+#print("Winner:", winnerName, " Score:", maxScore)
 
 
 
