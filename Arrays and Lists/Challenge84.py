62fd94badf8ad036dd657bbffbfbb2fad83148d7
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-07-20 17:00:30 +0100

    Updated Challenge 84

--- Arrays and Lists/Challenge84.py
+++ Arrays and Lists/Challenge84.py
@@ -3,17 +3,17 @@
 #list.
 
 
-booking = [ [57,68,100,124], [43,52,92,101], [72,78,84,90], [59,88,79,100] ]
+booking = [ [57,68,100,124], [43,52,92,101], [72,78,84,90], [0,0,0,0] ]
 siteList = ["Cambridge","Newcastle","Manchester","Oxford"]
 monthList = ["May","June","July","August"]
 
 print("Which site do you want to display bookings for?")
-site = int(input("Enter 0 for Cambridge, 1 for Newcastle, 2 for Manchester: "))
+site = int(input("Enter 0 for Cambridge, 1 for Newcastle, 2 for Manchester, 3 for Oxford: "))
 
 print("Which month do you want to display bookings for?")
 month = int(input("Enter 0 for May, 1 for June, 2 for July, 3 for August: "))
 
-print("Bookings for " + siteList[site] + " in " + monthList[monthList] + \
+print("Bookings for " + siteList[site] + " in " + monthList[month] + \
       " : " + str(booking[site][month]))  
 
       
