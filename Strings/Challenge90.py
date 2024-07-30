f7ccb253c22dccfd1efeb53da76ea63ddab95d58
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-07-30 19:02:13 +0100

    U[dated Challenge

---
+++ Strings/Challenge90.py
@@ -0,0 +1,7 @@
+#Write a program to ask the user to  enter a sentence. Calculate and print the number of times the letter 
+#"e" appears in the sentence.
+
+
+sentence_e = input("Write a sentence")
+
+if sentence_e 
