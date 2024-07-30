d17c7bf3a394394c92b735b243bcab09677b0643
Emeka Michael Nzeopara <michaels4uandme@gmail.com>
2024-07-30 21:17:01 +0300

    Created a quiz for the assignment

---
+++ Quiz1.py
@@ -0,0 +1,4 @@
+# Create a list of random numbers and inside the list, count any of the number that has 1 as one entity.
+# For example a list of 21,8,56,378,301,88,17,13,61
+# The number of "1" that occurs is 5
+
