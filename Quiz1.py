25e1b39667cb7dc4fd8e69ef72604e566ff331c0
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-08-08 17:13:44 +0100

    Updated Quiz

--- Quiz1.py
+++ Quiz1.py
@@ -2,3 +2,13 @@
 # For example a list of 21,8,56,378,301,88,17,13,61
 # The number of "1" that occurs is 5
 
+random_numbers = ["56","20","43","52","99","50","64","22","18","61","89","77"]
+
+count = 0
+
+for i in random_numbers:
+        if "1" in i:
+            count = count + 1
+
+
+print("The amount of numbers that have 1 in it are", count)
--- Snakegame.py
+++ Snakegame.py
@@ -1,36 +1,37 @@
-import random 
+#import random 
  
 
-GAME_WIDTH = 850
-GAME_HEIGHT = 850
-SPEED = 50
-SPACE_SIZE = 50
-BODY_PARTS = 3
-SNAKE_COLOR = "blue"
-FOOD_COLOR = "red"
-BACKGROUND_COLOR = "black"
+#GAME_WIDTH = 850
+#GAME_HEIGHT = 850
+#SPEED = 50
+#SPACE_SIZE = 50
+#BODY_PARTS = 3
+#SNAKE_COLOR = "blue"
+#FOOD_COLOR = "red"
+#BACKGROUND_COLOR = "black"
 
-class Snake:
-    pass
+#class Snake:
+#    pass
 
-class Food:
-    pass
+#class Food:
+#    pass
 
-def next_turn():
-    pass
+#def next_turn():
+#    pass
 
-def change_direction(new_direction):
-    pass
+#def change_direction(new_direction):
+#    pass
 
-def check_collisions():
-    pass
+#def check_collisions():
+#    pass
 
-def game_over():
-    pass
+#def game_over():
+#    pass
 
-import tkinter
-window = tkinter() 
-window.title("Snake Game")
-window.resizable(False, False)
+##import tkinter
+#window = tkinter() 
+#window.title("Snake Game")
+#window.resizable(False, False)
 
-window.mainloop()
+#window.mainloop()
+
