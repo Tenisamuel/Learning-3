dcab02c9d7145f84d40827a2aaa3d0e6b7e3fb6b
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-08-08 18:06:39 +0100

    Updated Challenge

--- Strings/Challenge93.py
+++ Strings/Challenge93.py
@@ -3,8 +3,8 @@
 #printing all those with third and fourth digits equal to 32"
 
 productCode = ["SN44-SHD","PS32-POR","HT60-BFS","SN32-UHD","SG32-SMT","SN55-4KS"]
-for index in range(1,7)
+for index in range(0,6):
     size = productCode[index]
-    .........................................
+    if index == 
 	............................
 	
