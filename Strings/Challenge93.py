a28e65b2d1dcc3aa3dd49f088a5ca4ee3e4c0d95
Emeka Michael Nzeopara <michaels4uandme@gmail.com>
2024-08-08 19:54:49 +0300

    Added the challenges

---
+++ Strings/Challenge93.py
@@ -0,0 +1,10 @@
+#Program name: Level 6 Challenge 93 Product numbers incomplete
+#This program processes a list of product numbers for televisions,
+#printing all those with third and fourth digits equal to 32"
+
+productCode = ["SN44-SHD","PS32-POR","HT60-BFS","SN32-UHD","SG32-SMT","SN55-4KS"]
+for index in range.................
+    size = productCode[index]
+    .........................................
+	............................
+	
