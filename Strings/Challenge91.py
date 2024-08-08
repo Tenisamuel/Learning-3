9359730a4935f66283b4200db67f924fa0ace2b4
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-08-08 16:27:49 +0100

    Updated Challenge 91

---
+++ Strings/Challenge91.py
@@ -0,0 +1,10 @@
+#Copy and save the above program. Add futher statements to prints the results of using string methods isalpha()
+#mystring.split(""), myString.Index(), mystring.replace(s1, s2)
+
+str_example = "abcdefghijklmnopqrstuvwxyz"
+
+result = str_example.isalpha()
+
+print(result)
+
+#All letters in the alphatbet have to be present to use this function (There have to also be no spaces.)
