78bd044d5e9c4d6a916882ec67f5bb484fdbcdd2
Emeka Michael Nzeopara <michaels4uandme@gmail.com>
2024-10-31 21:32:17 +0100

    New commit

--- Strings/Challenge93.py
+++ Strings/Challenge93.py
@@ -2,8 +2,9 @@
 #This program processes a list of product numbers for televisions,
 #printing all those with third and fourth digits equal to 32"
 
-productCode = ["SN44-SHD","PS32-POR","HT60-BFS","SN32-UHD","SG32-SMT","SN55-4KS"]
-for index in range(0,6):
-    size = productCode[index]
-    if size [2:4]
-        print()
+#productCode = ["SN44-SHD","PS32-POR","HT60-BFS","SN32-UHD","SG32-SMT","SN55-4KS"]
+#for index in range(0,6):
+#    size = productCode[index]
+#    if size [2:4]
+#        print()
+
