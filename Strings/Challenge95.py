154f34006a10ed2796b56200084d9c53fa007359
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-10-31 14:30:09 -0700

    updated

---
+++ Strings/Challenge95.py
@@ -0,0 +1,6 @@
+#Write a program which asks a user to enter a new password. The password must be at least 10 characters long
+#and the user is repeatedly asked to re-enter password if it is less than 10 characters.
+
+#Once the user has entered a valid password, the user is asked to verify it by entering it a second time.If it matches, 
+#the message "Password accepted" is pribted and the program terminates.
+
