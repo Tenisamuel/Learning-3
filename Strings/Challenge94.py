cf4bc21c3b0eaef9740f21e088710738290582f3
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-10-31 14:13:17 -0700

    Updated new Challenge 94

--- Strings/Challenge94.py
+++ Strings/Challenge94.py
@@ -5,13 +5,13 @@ book = "The book by Damian Barr is called 'You will be safe here'"
 
 #find the length of the string named book
 
-.........................................
+lengthofbook = len(book)
 
 #find the first single quote mark
+location_singlequote = book[34]
 
-.........................................
 
 # now slice the string to isolate the title
 
-.........................................
+title = book[34:58]
 print(title)
