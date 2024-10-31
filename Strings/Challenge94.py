cce0ee730eba86267db014e1d2d9c2d9aea811c1
Emeka Michael Nzeopara <michaels4uandme@gmail.com>
2024-10-31 21:59:00 +0100

    Added snippet of code to Challenge94.py

---
+++ Strings/Challenge94.py
@@ -0,0 +1,17 @@
+#Program name: Level 6 Challenge 94 Book title incomplete
+#This program locates a book title in a string and prints it
+
+book = "The book by Damian Barr is called 'You will be safe here'"
+
+#find the length of the string named book
+
+.........................................
+
+#find the first single quote mark
+
+.........................................
+
+# now slice the string to isolate the title
+
+.........................................
+print(title)
