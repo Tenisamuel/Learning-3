9d4061f8511bcf9c18a821ff265c901139dc9f16
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-11-02 12:05:01 -0700

    Updated Challenge

---
+++ Strings/Challenge98.py
@@ -0,0 +1,4 @@
+#The program code for Challenge 98 number incomplete contains the condition
+flightNumber = input("Enter flight number AA or BA followed by 4 digits: ")
+if not(flightNumber[0:2].upper() == "AA" or 
+       flightNumber [0:2].upper() == "BA")
