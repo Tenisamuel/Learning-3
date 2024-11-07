22ab6aef2ee0bf0497890a2431cf12bac135113f
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-11-07 20:37:40 +0000

    Updated Challenge 100

---
+++ Strings/Challenge100.py
@@ -0,0 +1,8 @@
+#Error message
+#Traceback (most recent call last):
+#   file "C:/Level Example 5 a runtime error.py", line 5, in <module>
+#      mystringUppercase = mystring.upper()
+#NameError: name 'mystring' is not defined
+
+#Answer
+#'myString'
