36f70f656ee3aed89bee97ba78c1a1b16b1610e2
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-11-12 16:25:28 +0000

    Updated Challenge98

--- Strings/Challenge98.py
+++ Strings/Challenge98.py
@@ -1,4 +1,8 @@
 #The program code for Challenge 98 number incomplete contains the condition
-flightNumber = input("Enter flight number AA or BA followed by 4 digits: ")
-if not(flightNumber[0:2].upper() == "AA" or 
-       flightNumber [0:2].upper() == "BA"):
+#Explain the reason for using the method upper() in this statement.
+#flightNumber = input("Enter flight number AA or BA followed by 4 digits: ")
+#if not(flightNumber[0:2].upper() == "AA" or 
+#       flightNumber [0:2].upper() == "BA"):
+
+#It is converting the string from small letters capital letters,just incase if the user inputs in small letters
+#And if the user inputs in Capital letters it just stays in Capital letters.
