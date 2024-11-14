effa7669c4aadbf2063c3fe46f2797cd921bc906
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-11-14 19:12:05 +0000

    Updated Snakegame

--- Snakegame.py
+++ Snakegame.py
@@ -1,37 +1,7 @@
-#import random 
- 
-
-#GAME_WIDTH = 850
-#GAME_HEIGHT = 850
-#SPEED = 50
-#SPACE_SIZE = 50
-#BODY_PARTS = 3
-#SNAKE_COLOR = "blue"
-#FOOD_COLOR = "red"
-#BACKGROUND_COLOR = "black"
-
-#class Snake:
-#    pass
-
-#class Food:
-#    pass
-
-#def next_turn():
-#    pass
-
-#def change_direction(new_direction):
-#    pass
-
-#def check_collisions():
-#    pass
-
-#def game_over():
-#    pass
-
-##import tkinter
-#window = tkinter() 
-#window.title("Snake Game")
-#window.resizable(False, False)
-
-window.mainloop()
-
+import random
+RPS = ["Rock" , "Paper" ,"Scissors"]
+for i in range(5):
+    userinputchoice = str(input("Enter Rock, Paper Or Scissors")).lower
+    Computerchoice = random.choices(RPS)
+    print(userinputchoice)
+    print(Computerchoice)
