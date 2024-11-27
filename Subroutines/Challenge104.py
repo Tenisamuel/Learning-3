70dbdc16ac69237991e04bea311b7ae418cd1e0a
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-11-27 20:38:00 +0000

    Updated First Challenge of sub routines]

---
+++ Subroutines/Challenge104.py
@@ -0,0 +1,7 @@
+#Write a statement which calls the built in function round() to round price1. a floating point variable
+#Containing 5.327, to two decimal places and assign the result to price2. What is the value of price2 after
+#the statement has been executed.
+
+price1 = 5.327
+price2 = round(price1, 2)
+print(price2)
