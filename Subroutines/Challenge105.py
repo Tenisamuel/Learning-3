729e0966e65f5b88377963b08a40f42689e56a71
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-11-27 20:50:36 +0000

    Updated Challenge 105

---
+++ Subroutines/Challenge105.py
@@ -0,0 +1,8 @@
+#write a statement using the ord() function to convert the ASCII / Unicode character 'A' to its equivalent integer
+#65 and print it.
+
+#write a statement using the chr() to convert the integer 68 to its equivalent ASCII character and print it what us
+#printed?
+
+print(ord('A'))
+print(chr(68))
