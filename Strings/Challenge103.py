4bf89f59ce3ad716531f55e76bf64c4bab70aa33
Emeka Michael Nzeopara <michaels4uandme@gmail.com>
2024-11-27 21:23:34 +0100

    Solution to the question

--- Strings/Challenge103.py
+++ Strings/Challenge103.py
@@ -5,31 +5,34 @@ print("The list below shows the types of strawberry plants in stock.")
 
 #display options:
 
-print("        Variety")
+print("Number  Variety")
 print("{:^8d}{:<15}".format(1, "Christine"))
 print("{:^8d}{:<15}".format(2, "Vibrant"))
 print("{:^8d}{:<15}".format(3, "Honeoye"))
 print("{:^8d}{:<15}".format(4, "Cambridge"))
-print("{:^8d{:<15}}".format(5, "Pegasus"))
+print("{:^8d}{:<15}".format(5, "Pegasus"))
       
-#input selected variety. (Must be between 1 and 5, or 0 to end)
+#input selected variety. (Must be between 1 and 5, or 0 to end) 
 
-chooseAgain = False
-while chooseAgain == True:
+
+
+chooseAgain = True
+while chooseAgain:
     variety = input("Select variety (1-5) to view description (0 to end): ")
-    if  not(variety in (.......................)):
+    if  not(variety in ("0","1","2","3","4","5")):
         print ("Invalid choice")
     elif variety == "0":
-        ..........................    
+        chooseAgain = False
     else:          
-        if variety ==1 or variety == 2:
+        if variety == "1" or variety == "2":
             print("Christine and Vibrant - Ready to pick early June")
             print("Large, sweet and delicious")
-        elif variety == 3:
+        elif variety == "3":
             print("Honeoye - Ready to pick from mid-June")
             print("Good for jam-making")
-        elif variety == 4 or ..................
+        elif variety == "4" or variety == "5":
             print("Cambridge and Pegasus - Juicy and soft")
             print("Pick late June to late July")
         
 print("Thank you - more choices available soon!")
+
