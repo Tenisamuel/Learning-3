9d4a80d84b5fa720e88baecb6672774562f83b6b
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-11-27 20:22:34 +0000

    Updated Challenge 103

--- Strings/Challenge103.py
+++ Strings/Challenge103.py
@@ -5,31 +5,31 @@ print("The list below shows the types of strawberry plants in stock.")
 
 #display options:
 
-print("        Variety")
+print("Number and Varietys")
 print("{:^8d}{:<15}".format(1, "Christine"))
 print("{:^8d}{:<15}".format(2, "Vibrant"))
 print("{:^8d}{:<15}".format(3, "Honeoye"))
 print("{:^8d}{:<15}".format(4, "Cambridge"))
-print("{:^8d{:<15}}".format(5, "Pegasus"))
-      
-#input selected variety. (Must be between 1 and 5, or 0 to end)
+print("{:^8d}{:<15}".format(5, "Pegasus"))
 
-chooseAgain = False
-while chooseAgain == True:
-    variety = input("Select variety (1-5) to view description (0 to end): ")
-    if  not(variety in (.......................)):
-        print ("Invalid choice")
-    elif variety == "0":
-        ..........................    
-    else:          
-        if variety ==1 or variety == 2:
-            print("Christine and Vibrant - Ready to pick early June")
-            print("Large, sweet and delicious")
-        elif variety == 3:
-            print("Honeoye - Ready to pick from mid-June")
-            print("Good for jam-making")
-        elif variety == 4 or ..................
-            print("Cambridge and Pegasus - Juicy and soft")
-            print("Pick late June to late July")
+#input selected variety. (Must be between 1 and 5, or 0 to end)
+#
+#chooseAgain = False
+#while chooseAgain == True:
+#    variety = input("Select variety (1-5) to view description (0 to end): ")
+#    if  not(variety in ("0","1","2","3","4","5")):
+#        print ("Invalid choice")
+#    elif variety == "0":
+#        ..........................    
+#    else:          
+#        if variety ==1 or variety == 2:
+#            print("Christine and Vibrant - Ready to pick early June")
+#            print("Large, sweet and delicious")
+#        elif variety == 3:
+#            print("Honeoye - Ready to pick from mid-June")
+#            print("Good for jam-making")
+#        elif variety == 4 or variety == 5:
+#            print("Cambridge and Pegasus - Juicy and soft")
+#            print("Pick late June to late July")
         
-print("Thank you - more choices available soon!")
+#print("Thank you - more choices available soon!")
