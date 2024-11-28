5246a0faf1befa59662ec0a19f214af818c04342
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-11-28 19:12:05 +0000

    Update Challenge 109

---
+++ Subroutines/Challenge109.py
@@ -0,0 +1,5 @@
+#Write a program to ask the user to enter the area of ocean (in square nautical miles) scheduled for cleanup each
+#day to clear it for plastic pollution.One vessel can cover 12 square nautical miles per day. Calculate the smallest
+#whole number of vessels rewuired to operate the cleanup.
+
+areaprompt = int(input("Enter the area of the ocean (in square meters):"))
