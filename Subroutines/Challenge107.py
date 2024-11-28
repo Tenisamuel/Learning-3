d0a07d2e3afe44491477da8e2a5755b83cdfc851
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-11-28 18:30:49 +0000

    Updated Challenge 107

---
+++ Subroutines/Challenge107.py
@@ -0,0 +1,6 @@
+#Write statements using the math.pi constant to calculate and print, correct to two decimal places,
+#the circumference of a circle with radius5.8.(use the formula circumference 2 pie r)
+
+import math
+circumference = 2 * math.pi * 5.8
+print(round(circumference, 2)) 
