f15394c44979b66127525bc344e557b2d89cff24
Emeka Michael Nzeopara <michaels4uandme@gmail.com>
2024-11-30 16:02:27 +0100

    Added the challenge 111 snippet of code

---
+++ Subroutines/Challenge111.py
@@ -0,0 +1,18 @@
+#Program name: Level 7 Challenge 111 Timer incomplete
+#Program asks user to set a timer for a number of seconds between 1 and 5
+
+import time
+waitTime = int(input("How many seconds (between 1 and 5) \
+do you want to set the timer for? "))
+
+#validate the user entry
+#If not between 1 and 5, print message and ask user to re-enter
+
+................................
+    .......................................    
+
+print("Start the timer...")
+............................
+
+#wait the required number of seconds before printing
+print("Time's up!")
