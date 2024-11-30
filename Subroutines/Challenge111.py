6936b6bda97c228133bb71534731163b7306030e
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-11-30 15:23:50 +0000

    Updated Challenge 111

--- Subroutines/Challenge111.py
+++ Subroutines/Challenge111.py
@@ -8,8 +8,9 @@ do you want to set the timer for? "))
 #validate the user entry
 #If not between 1 and 5, print message and ask user to re-enter
 
-................................
-    .......................................    
+if not (waitTime in (1,2,3,4,5)):
+    print("Re-enter")
+    
 
 print("Start the timer...")
 ............................
