a2e23f28a7179b1a851328ccf20b08f619492ab1
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-12-05 18:47:54 +0000

    Updated Automation project

--- Automationproject.py
+++ Automationproject.py
@@ -1,15 +1,14 @@
-from pytube import YouTube
-from sys import argv
+#from pytube import YouTube
+#from sys import argv
 
-link = argv[1]
-yt = YouTube(link)
+#link = argv[1]
+#yt = YouTube(link)
 
-print("Title: ", yt.title)
+#print("Title: ", yt.title)
 
-print("View: ", yt.views)
+#print("View: ", yt.views)
 
-yd = yt.streams.get_highest_resolution()
+#yd = yt.streams.get_highest_resolution()
 
-yd.download("C:\Users\HP\OneDrive\Pictures\Screenshots")
-
+#yd.download("C:\Users\HP\OneDrive\Pictures\Screenshots")
 
