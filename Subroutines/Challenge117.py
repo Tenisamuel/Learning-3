5cb3500fb1ccb6011b33e0dae27a23b4332425aa
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-12-20 21:39:09 +0000

    Updated challenge 117

---
+++ Subroutines/Challenge117.py
@@ -0,0 +1,9 @@
+#Load the program Challenge 117 local and global variables from the challenge solutions\ level 7 folder
+#Examine the code and run the program
+
+#Name two global variables used in the program.
+
+#Name four local variables used in the function simulation()
+
+#Explain the effect on the execution of the program of uncommenting the statement print(face) in the main 
+#Program.
