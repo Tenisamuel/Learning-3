4271bf713b956e18d8ad921636ff90d7005b187b
Emeka Michael Nzeopara <50933358+Los-merengue@users.noreply.github.com>
2024-12-20 22:40:28 +0100

    added a code

--- Subroutines/Challenge117.py
+++ Subroutines/Challenge117.py
@@ -7,3 +7,30 @@
 
 #Explain the effect on the execution of the program of uncommenting the statement print(face) in the main 
 #Program.
+
+
+
+import random
+def simulation():
+    global title
+    title = "Game number 1"
+    #define a list named face containing 6 zeros in face[0], face[1] to face[6]   
+    #face[0] will not be used
+    face = [0]*7
+    for n in range(6):
+        #generate a random number in the range 1-6
+        dieRoll = random.randint(1,6)
+        #add 1 to the number of times that face appeared
+        face[dieRoll] = face[dieRoll] + 1 
+    print(heading)
+    for dieface in range(1,7):
+          print(dieface," was thrown",face[dieface], "times")
+
+#****************** main program *********************
+title = "Game number 1"
+print(title)
+heading = "Dieroll simulation"
+simulation()
+
+#What is the effect of uncommenting the statement below?
+#print(face)
