d937b73429e7f1478303f950086da4c10557032c
Tenisamuel <teniolaolanrewaju09@gmail.com>
2024-12-20 21:28:50 +0000

    Updated Challeges

--- Subroutines/Challenge116.py
+++ Subroutines/Challenge116.py
@@ -6,3 +6,17 @@
 #IS 7261 SECONDS.
 
 #What happens if you enter 211 instead of 020101.
+
+#It gives you an error
+
+hours_min_seconds = input("Enter the time of the runner in format hhmmss")
+
+slice1 = hours_min_seconds[0:2]
+slice2 = hours_min_seconds[2:4] 
+slice3 = hours_min_seconds[4:6] 
+
+int_slice1 = int(slice1) * 3600
+int_slice2 = int(slice2) * 60
+int_slice3 = int(slice3)
+
+print (int_slice1+int_slice2+int_slice3)
