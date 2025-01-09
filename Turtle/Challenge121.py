feafcfe1f4ec090b4c9d00e698150cf57ebd3e53
Tenisamuel <teniolaolanrewaju09@gmail.com>
2025-01-09 18:17:11 +0000

    Updated Challenge 121

---
+++ Turtle/Challenge121.py
@@ -0,0 +1,12 @@
+#copy the program given in Example 1. Then change the pen colour to green ,pen width
+
+import turtle
+
+turtle.pensize(10)
+turtle.pencolor("pink")
+
+for n in range(6):
+    turtle.forward(60)
+    turtle.right(60)
+
+
