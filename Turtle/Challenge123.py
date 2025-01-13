b1927d76ddb3e057fa08103609f424baf1b7fce8
Emeka Michael Nzeopara <50933358+Los-merengue@users.noreply.github.com>
2025-01-13 18:51:24 +0100

    Added the snippet of code for challenge123.py

---
+++ Turtle/Challenge123.py
@@ -0,0 +1,12 @@
+#program name: Level 8 Challenge 123 Draw red shape incomplete
+import turtle
+
+turtle.pensize(10)
+turtle.pencolor("red")
+turtle.shape("turtle")
+turtle.speed("slow")
+
+#draw the shape
+turtle.forward(200)
+
+#complete the drawing
