81671c536a09a90945b7b21a8ada549b5f728011
Emeka Michael Nzeopara <50933358+Los-merengue@users.noreply.github.com>
2025-01-14 20:51:19 +0100

    Added the script to the Challenge124.py

---
+++ Turtle/Challenge124.py
@@ -0,0 +1,27 @@
+#Program name: Challenge 124 draw blue shape
+import turtle
+
+#set initial size, colour and position of the pen.
+turtle.pensize(5)
+turtle.pencolor("blue")
+turtle.penup()
+turtle.setpos(-100,-100)
+turtle.pendown()
+turtle.right(180)
+
+#start the drawing
+turtle.forward(300)
+turtle.right(90)
+turtle.forward(200)
+turtle.right(90)
+turtle.forward(300)
+
+turtle.penup()
+turtle.setpos(-300,0)
+turtle.pendown()
+
+turtle.forward(300)
+turtle.left(90)
+turtle.forward(200)
+turtle.left(90)
+turtle.forward(300)
