f95d8e608f19b37882eba7be4c596b5a1cb8f040
Emeka Michael Nzeopara <50933358+Los-merengue@users.noreply.github.com>
2025-01-14 20:56:49 +0100

    added

--- Turtle/Challenge123.py
+++ Turtle/Challenge123.py
@@ -7,24 +7,9 @@ turtle.shape("turtle")
 turtle.speed("slow")
 
 #draw the shape
-#for i in range(8):
+for i in range(8):
+    turtle.forward(100)
+    turtle.left(45)
 
-turtle.forward(200)
-turtle.right(90)
-turtle.forward(100)
-turtle.left(90)
-turtle.forward(200)
-turtle.right(90)
-turtle.forward(200)
-turtle.right(90)
-turtle.forward(100)
-turtle.right(90)
-turtle.forward(100)
-turtle.left(90)
-turtle.forward(200)
-turtle.right(90)
-turtle.forward(100)
-turtle.left(90)
-turtle.forward(100)
 
 #complete the drawing
