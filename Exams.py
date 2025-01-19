74e58121b8a5c4d6a64e6930d12c34cc40b17ec2
Tenisamuel <teniolaolanrewaju09@gmail.com>
2025-01-19 14:02:44 +0000

    Updated all

--- Exams.py
+++ Exams.py
@@ -17,35 +17,39 @@
 #print(aveargerainfall)
 #print(monthsaboveaverage)
 
-months = ["January","Febuary","March","April","May","june","july","august","september","october","november","December"]
-rainfall_list = []
-annualrainfall = 0
-for i in range(len(months)-1):
-    print("Insert the input rainfall for the month of", months[i])
-    rainfall = int(input("Please input rainfall in the last month"))
-    rainfall_list.append(rainfall)
-    annualrainfall = annualrainfall + rainfall
-averagerainfall = annualrainfall / 12
-monthsaboveaverage = 0
-for i in range(len(months)-1):
-    if rainfall_list[i] > averagerainfall:
-        monthindicate = []
-        monthindicate.append(rainfall_list[i])
-        monthsaboveaverage = monthsaboveaverage + 1
-    print(months[i] + ": " + str(rainfall_list[i]))
-print(annualrainfall)
-print(averagerainfall)
-print(monthsaboveaverage)
-print("These are the months above average", monthindicate)
+#months = ["January","Febuary","March","April","May","june","july","august","september","october","november","December"]
+#rainfall_list = []
+#annualrainfall = 0
+#for i in range(len(months)-1):
+#    print("Insert the input rainfall for the month of", months[i])
+#    rainfall = int(input("Please input rainfall in the last month"))
+#    rainfall_list.append(rainfall)
+#    annualrainfall = annualrainfall + rainfall
+#averagerainfall = annualrainfall / 12
+#monthsaboveaverage = 0
+#for i in range(len(months)-1):
+#    if rainfall_list[i] > averagerainfall:
+#        monthindicate = []
+#        monthindicate.append(rainfall_list[i])
+#        monthsaboveaverage = monthsaboveaverage + 1
+#    print(months[i] + ": " + str(rainfall_list[i]))
+#print(annualrainfall)
+#print(averagerainfall)
+#print(monthsaboveaverage)
+#print("These are the months above average", monthindicate)
 
+#marksFile = open("exams.txt", 'w')
+#for n in range (20):
+#    name = input("Enter student name: ")
+#    mark = input("Enter student mark: ")
+#    marksFile.write(name + "," + mark + "\n")
+#marksFile.close()
 
-
-
-
-
-
-
-
-
+with open("exams.txt", "r") as file:
+    for line in file:
+        line = file.readlines()
+        print(line, end = "")
+print("End of file reached")
+file.close()
 
 
