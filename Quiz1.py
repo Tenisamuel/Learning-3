210136996c817d9730251cc6af2c6683ca6a75bf
Tenisamuel <teniolaolanrewaju09@gmail.com>
2025-02-02 12:14:03 +0000

    updated

--- Quiz1.py
+++ Quiz1.py
@@ -13,5 +13,53 @@
 
 #print("The amount of numbers that have 1 in it are", count)
 
-import turtle
-window = turtle.screen
+#import turtle
+#window = turtle.screen
+
+
+#def sum(a, b):
+#    total = a + b
+#    return total
+
+
+
+
+#two_numbers = int(input("Enter a number"))
+#list = []
+
+#for i in range(two_numbers):
+#    list1 = list.append(int(input()))
+
+#sum_list1 = 0
+#for i in list:
+#    sum_list1 = sum_list1 + i
+
+
+
+#print("Calculated numbers are", sum_list1)
+#first_number = int(input("Enter a number: "))
+#second_number = int(input("Enter second number: "))
+
+#calculation = first_number + second_number
+
+#print("This is your two numbers summed together", calculation)
+
+
+print("This program will either help you multiply,subtract or add")
+
+m_s_a = input("Would you like to multiply,subtract or add\n")
+
+number1 = int(input("Enter a number: "))
+number2 = int(input("Enter a number: "))
+
+if m_s_a == "multiply":
+    num = number1 * number2
+    print(num)
+elif m_s_a == "subtract":
+    num2 = number1 - number2
+    print(num2)
+elif m_s_a == "add":
+    num3 = number1 + number2
+    print(num3)
+else:
+    print("please satisfy the condition")
