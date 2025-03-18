c7d6157d625d8907d333c82f6ab186e7b0771d23
Tenisamuel <teniolaolanrewaju09@gmail.com>
2025-03-18 20:24:59 +0000

    First draft of challenge129

---
+++ Turtle/Challenge129.py
@@ -0,0 +1,9 @@
+#Write a statement which calls the function drawpolygon() to draw an equilateral triangle with sides
+#of length 100.
+
+
+
+def drawpolygon(numSides, sidelength):
+    interiorAngle = (100 - 2) * 180 /  3
+    turnAngle = 180 - interiorAngle
+
