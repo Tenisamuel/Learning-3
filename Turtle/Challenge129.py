51be305b2d0151bfa7cb6bb2998d7dbc22882d21
Tenisamuel <teniolaolanrewaju09@gmail.com>
2025-03-24 19:11:28 +0000

    update

--- Turtle/Challenge129.py
+++ Turtle/Challenge129.py
@@ -3,7 +3,21 @@
 
 
 
-def drawpolygon(numSides, sidelength):
-    interiorAngle = (100 - 2) * 180 /  3
-    turnAngle = 180 - interiorAngle
-drawpolygon()
+#import turtle
+
+#def drawpolygon(numSides, sidelength):
+#    interiorAngle = (100 - 2) * 180 /  3
+#    turnAngle = 180 - interiorAngle
+#drawpolygon()
+
+import turtle
+
+def drawpolygon(sides, length):
+    for _ in range(sides):
+        turtle.forward(length)
+        turtle.left(120)  # Turning left by 120 degrees for an equilateral triangle
+
+drawpolygon(3, 100)  # Draws an equilateral triangle with side length 100
+
+turtle.done()
+
