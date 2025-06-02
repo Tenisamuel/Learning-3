f72e231dcd533fb45b5741fdb1a5023555f9253c
Tenisamuel <teniolaolanrewaju09@gmail.com>
2025-06-02 21:42:00 +0100

    Updating dice game

--- Snakegame.py
+++ Snakegame.py
@@ -3,11 +3,12 @@ import random
 def roll():
     min_value = 1
     max_value = 6
-    roll = random.randint(min_value, max_value )
+    roll = random.randint(min_value, max_value)
 
     return roll
+
 while True: 
-    players = input("Enter a value between 1 - 4")
+    players = input("Enter a value between (2 - 4): ")
     if players.isdigit():
         players = int(players)
         if 2 <= players <= 4:
@@ -20,9 +21,29 @@ while True:
 max_score = 50
 player_scores = [0 for _ in range(players)]
 
-while max(player_scores < max_score):
-    should_roll = input("Do you want to roll? (y)")
-    if should_roll.lower() == "y":
-        break
-    
-    value = roll()
+while max (player_scores) < max_score:
+
+    for player_idx  in range(players):
+        print("\n Player number", player_idx + 1, "turn has just started!\n")
+        current_score = 0
+
+        while True:
+            should_roll = input("Do you want to roll? (y)?")
+            if should_roll.lower() != "y":
+                break
+
+  
+            value = roll()
+            if value == 1:
+                print("You rolled a 1! - Turn done")
+                current_score = 0
+                break
+            else:
+                current_score = value
+                print("Your rolled a", value)
+            
+            print("your score is:", current_score)
+
+
+        player_scores[player_idx] == current_score
+        print("Your total score is:", player_scores[player_idx])
