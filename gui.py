5f62d9e01ad667afb37d2c982de34db1e074b89b
Tenisamuel <teniolaolanrewaju09@gmail.com>
2026-04-25 21:39:35 +0100

    Update latest changes

---
+++ gui.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+#widgets: designed to make buttons, windows, interfaces, menus, pointers.
+#windows: a container to hold the widgets.
+
+window = tk.Tk() #creates a window object
+window.title("Xnec") #sets the title of the window
+
+window.mainloop()
